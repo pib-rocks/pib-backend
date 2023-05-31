7b3a6855e396bebef377a6ddf12d875f5edd158f
Aleksandr Vdovin <aleksandr.vdovin@isento.de>
2023-05-31 14:35:08 +0200

    init script for database added

---
+++ setup_files/cerebra_init_query.sql
@@ -0,0 +1,169 @@
+create table IF NOT EXISTS MOTOR (id integer primary key AUTOINCREMENT NOT NULL, Name Text NOT NULL, PulseMin Integer NOT NULL, PulseMax Integer NOT NULL, DegreeMin Integer NOT NULL, DegreeMax Integer NOT NULL, Velocity Integer NOT NULL);
+insert into
+   MOTOR 
+values
+   (
+      'tilt_forward_motor', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'tilt_sideways_motor', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'thumb_left_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'index_left_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'middle_left_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'ring_left_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'pinky_left_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'thumb_right_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'index_right_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'middle_right_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'ring_right_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'pinky_right_stretch', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'upper_arm_left_rotation', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'ellbow_left', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'lower_arm_left_rotation', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'wrist_left', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'shoulder_vertical_left', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'shoulder_horizontal_left', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'upper_arm_right_rotation', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'ellbow_right', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'lower_arm_right_rotation', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'wrist_right', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'shoulder_vertical_right', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
+insert into
+   MOTOR 
+values
+   (
+      'shoulder_horizontal_right', 0 , 0, 0, 0, 0, 0, 0, 0
+   )
+;
