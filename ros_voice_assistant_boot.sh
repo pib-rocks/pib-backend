d0920b162414b16a1e244d5ff753a156a96c70ee
Lisa-isento <144238545+Lisa-isento@users.noreply.github.com>
2023-09-12 11:44:07 +0200

    fix : boot.sh
    Edit to have correct paths and names

--- ros_voice_assistant_boot.sh
+++ ros_voice_assistant_boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-source /home/pib/ros_working_dir/voice_assistant/install/setup.bash
-ros2 run voice-assistant assistant
+source /home/pib/ros_working_dir/src/voice_assistant/install/setup.bash
+ros2 run voice_assistant assistant
