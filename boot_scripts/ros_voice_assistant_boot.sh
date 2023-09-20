90993731e387ce79b127b31652cf8484a3fb77de
Lisabeth Hasse <lisabeth.hasse@isento.de>
2023-09-20 12:00:12 +0200

    fix(PR-312): edit boot.service file structure

--- boot_scripts/ros_voice_assistant_boot.sh
+++ boot_scripts/ros_voice_assistant_boot.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-source /home/pib/ros_working_dir/src/voice_assistant/install/setup.bash
-ros2 run voice_assistant assistant
+source /home/pib/ros_working_dir/install/setup.bash
