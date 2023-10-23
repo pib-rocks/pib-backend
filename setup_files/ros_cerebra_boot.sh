02056a62646ac75c56f94aa2a821f203078713ff
niklasIsento <144774775+niklasIsento@users.noreply.github.com>
2023-10-23 13:16:34 +0200

    fix(PR-349): source working dir

--- setup_files/ros_cerebra_boot.sh
+++ setup_files/ros_cerebra_boot.sh
@@ -2,4 +2,5 @@
 source /home/pib/ros_working_dir/ros_config.sh
 sudo service nginx restart
 source /opt/ros/humble/setup.bash
+source /home/pib/ros_working_dir/install/setup.bash
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml
