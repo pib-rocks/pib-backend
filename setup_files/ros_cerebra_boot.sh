d98f4f43d93e032e66f2e6620c8ab4cd44bfa37b
Christopher <christopher.fries@isento.de>
2023-10-23 10:09:08 +0200

    fix(PR-331): Fix Localhost environment for boot-scripts

--- setup_files/ros_cerebra_boot.sh
+++ setup_files/ros_cerebra_boot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/pib/ros_working_dir/ros_config.sh
 sudo service nginx restart
 source /opt/ros/humble/setup.bash
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml
