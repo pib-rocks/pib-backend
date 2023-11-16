bbaf95ad1f1e6f21ba8659abe44c1642ce4e0397
JaHe-isento <jan.hermann@isento.de>
2023-11-16 16:46:09 +0100

    fix(PR-367): Execute health check at the end of setup

--- setup-pib.sh
+++ setup-pib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script assumes:
-#   - that Ubuntu Desktop 22.04.2 is installed
+#   - that Ubuntu Desktop 22.04 is installed
 #   - the default-user "pib" is executing it
 #
 UBUNTU_VERSION=$(lsb_release -rs)
@@ -35,7 +35,7 @@ PIB_API_URL_PATH="https://github.com/pib-rocks/pib-api/archive/refs/heads/main.z
 #
 ROS_PACKAGES_LINK="https://raw.githubusercontent.com/pib-rocks/ros-packages/main/packages-set-up.sh"
 ROS_UPDATE_LINK="https://raw.githubusercontent.com/pib-rocks/setup-pib/main/update-pib.sh"
-ROS_HEALTH_LINK="https://raw.githubusercontent.com/pib-rocks/setup-pib/test-script/health-check-pib.sh"
+ROS_HEALTH_LINK="https://raw.githubusercontent.com/pib-rocks/setup-pib/main/health-check-pib.sh"
 #
 # We make sure that this script is run by the user "pib"
 if [ "$(whoami)" != "pib" ]; then
@@ -238,4 +238,4 @@ echo -e '\nPlease restart the system to apply changes...'
 cd $USER_HOME
 wget -O health-check-pib.sh $ROS_HEALTH_LINK
 chmod 755 health-check-pib.sh
-./test-script-pib.sh
+./health-check-pib.sh
