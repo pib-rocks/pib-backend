1e5e540e6ec187b9844e9a2460f4cc7d81d8cee3
JaHe-isento <jan.hermann@isento.de>
2023-11-20 11:03:06 +0100

    feat(PR-367): Implement partial health check options

--- health-check-pib.sh
+++ health-check-pib.sh
@@ -10,6 +10,7 @@ ubuntu_version=$(grep VERSION_ID "$os_release_info_filepath" | cut -f2 -d'=')
 
 # Variables for "echo -e" output text formatting
 red_text_color="\e[31m"
+yellow_text_color="\e[33m"
 reset_text_color="\e[0m"
 new_line="\n"
 
@@ -23,104 +24,147 @@ help_function()
     exit 1
 }
 
+# Input variables
+true="true"
+false="false"
+
 # Check if script was started in dev-mode
-is_dev_mode="false"
+is_dev_mode=$false
 if [ "$1" = "-d" ] 
 then
-    is_dev_mode="true"
+    is_dev_mode=$true
 # Show help if any other option was called
 elif [ -n "$1" ] 
 then
     help_function
 fi
 
-# Check the system variables
-echo -e $new_line"Checking system settings and installations."$new_line"These checks can take some time, so please lean back and wait :)"$new_line
+run_ubuntu_check=$true
+run_python_package_check=$true
+run_pip_package_check=$true
 
-if [ $USER != $expected_user_name ]; then
-    echo -e $red_text_color"Your user name is not $expected_user_name."$new_line"Please change your user name or switch to a user named $expected_user_name."$reset_text_color
-    exit 1
+if [ $is_dev_mode = $true ]; then
+    # Ask if the user wants to run a ubuntu setup and installation check
+    while true; do
+        read -rep $'\nDo you want to check the general ubuntu setup and installations? \nAnswer with yes or no: ' user_answer
+        case $user_answer in
+            [Yy]* ) break;;
+            [Nn]* ) run_ubuntu_check=$false; break;;  
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+    # Ask if the user wants to check python packages
+    while true; do
+        read -rep $'\nDo you want to check the python packages? \nAnswer with yes or no: ' user_answer
+        case $user_answer in
+            [Yy]* ) break;;
+            [Nn]* ) run_python_package_check=$false; break;;  
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+    # Ask if the user wants to check pib packages and services
+    while true; do
+        read -rep $'\nDo you want to check on pib packages and services? \nAnswer with yes or no: ' user_answer
+        case $user_answer in
+            [Yy]* ) break;;
+            [Nn]* ) run_pip_package_check=$false; break;;  
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
 fi
-echo -e "You're using the correct user name: " $expected_user_name$new_line
 
-if [ $ubuntu_version == $expected_ubuntu_version ]; then
-    echo -e "You're using the recommended Ubuntu version."$new_line
-else
-    echo -e $red_text_color"This Ubuntu version is not recommended to use with pib!"$reset_text_color
-fi
+# Check general Ubuntu setup and installation 
+if [ $run_ubuntu_check = $true ]; then
+    # Check the system variables
+    echo -e $new_line$yellow_text_color"--- checking ubuntu system settings and installations ---"$reset_text_color
 
-# Check system installations
-if ! colcon version-check >/dev/null 2>&1; then
-    echo -e $red_text_color"The colcon package is not installed"$reset_text_color
-fi
-
-INSTALLATIONS=([1]=python3-pip [2]=git [3]=python3 [4]=curl [5]=openssh-server [6]=software-properties-common [7]=unzip [8]=sqlite3 [9]=locales [10]=libusb-1.0-0 [11]=libudev1 [12]=procps [13]=php8.1-fpm [14]=python3-pyqt5 [15]=python3-pyqt5.qtopengl [16]=python3-serial [17]=python3-tz [18]=python3-tzlocal [19]=libusb-1.0-0-dev [20]=flac)
-
-for installation in "${INSTALLATIONS[@]}"
-do
-    if ! dpkg-query -W -f='${Status}' $installation 2>/dev/null | grep -q "install ok installed"; then
-        echo -e $red_text_color"The package $installation is not installed"$reset_text_color
-    else
-        echo -e $installation "checked"
+    if [ $USER != $expected_user_name ]; then
+        echo -e $red_text_color"Your user name is not $expected_user_name."$new_line"Please change your user name or switch to a user named $expected_user_name."$reset_text_color
+        exit 1
     fi
-done
+    echo -e "You're using the correct user name: " $expected_user_name$new_line
+
+    if [ $ubuntu_version == $expected_ubuntu_version ]; then
+        echo -e "You're using the recommended Ubuntu version."$new_line
+    else
+        echo -e $red_text_color"This Ubuntu version is not recommended to use with pib!"$reset_text_color
+    fi
+
+    # Check system installations
+    if ! colcon version-check >/dev/null 2>&1; then
+        echo -e $red_text_color"The colcon package is not installed"$reset_text_color
+    fi
+
+    INSTALLATIONS=([1]=python3-pip [2]=git [3]=python3 [4]=curl [5]=openssh-server [6]=software-properties-common [7]=unzip [8]=sqlite3 [9]=locales [10]=libusb-1.0-0 [11]=libudev1 [12]=procps [13]=php8.1-fpm [14]=python3-pyqt5 [15]=python3-pyqt5.qtopengl [16]=python3-serial [17]=python3-tz [18]=python3-tzlocal [19]=libusb-1.0-0-dev [20]=flac)
+
+    for installation in "${INSTALLATIONS[@]}"
+    do
+        if ! dpkg-query -W -f='${Status}' $installation 2>/dev/null | grep -q "install ok installed"; then
+            echo -e $red_text_color"The package $installation is not installed"$reset_text_color
+        else
+            echo -e $installation "checked"
+        fi
+    done
+    echo -e $yellow_text_color"--- ubuntu check completed ---"$reset_text_color
+fi
 
 # Check python package installations
-PIP_PACKAGES=([1]=depthai [2]=tinkerforge [3]=openai [4]=google-cloud-speech [5]=google-cloud-texttospeech [6]=pyaudio [7]=opencv-python [8]=setuptools)
-for package in "${PIP_PACKAGES[@]}"
-do
-    if ! pip show $package >/dev/null 2>&1; then
-        echo -e $red_text_color"The Python-Package $package is not installed"$reset_text_color
-    else
-        echo -e $package" checked"
-    fi
-done
+if [ $run_python_package_check = $true ]; then
+    echo -e $new_line$yellow_text_color"--- checking python packages ---"$reset_text_color
 
-echo -e $new_line"---python installations checked---"
-
-# Ask if the user wants to check pib packages and services
-while true; do
-    read -rep $'\nDo you want to continue with a check on pib packages and services? \nAnswer with yes or no: ' user_answer
-    case $user_answer in
-        [Yy]* ) break;;
-        [Nn]* ) exit 1;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+    PIP_PACKAGES=([1]=depthai [2]=tinkerforge [3]=openai [4]=google-cloud-speech [5]=google-cloud-texttospeech [6]=pyaudio [7]=opencv-python [8]=setuptools)
+    for package in "${PIP_PACKAGES[@]}"
+    do
+        if ! pip show $package >/dev/null 2>&1; then
+            echo -e $red_text_color"The Python-Package $package is not installed"$reset_text_color
+        else
+            echo -e $package" checked"
+        fi
+    done
+    echo -e $yellow_text_color"--- python package check completed ---"$reset_text_color
+fi
 
 # Check pib packages and services
-ROS_WORKING_DIR_SRC="/home/pib/ros_working_dir/src"
+if [ $run_pip_package_check = $true ]; then
+    echo -e $new_line$yellow_text_color"--- checking pib packages and services ---"$reset_text_color
 
-FOLDERS=([1]=motors [2]=datatypes [3]=voice-assistant [4]=ros2_oak_d_lite)
-PACKAGE_NAMES=([1]="name: motors" [2]="name: datatypes" [3]="name: voice_assistant" [4]="name: oak_d_lite")
-SYS_CTLS=([1]=ros_motor_control_node_boot.service [2]=ros_motor_current_node_boot.service [3]=pib_api_boot.service [4]=ros_camera_boot.service [5]=ros_cerebra_boot.service [6]=ros_voice_assistant_boot.service)
+    ROS_WORKING_DIR_SRC="/home/pib/ros_working_dir/src"
 
-# Change directory to make the colcon command work independently of the location of this shell script
-cd $ROS_WORKING_DIR_SRC
-COLCON_INFO=$(colcon info)
+    FOLDERS=([1]=motors [2]=datatypes [3]=voice-assistant [4]=ros2_oak_d_lite)
+    PACKAGE_NAMES=([1]="name: motors" [2]="name: datatypes" [3]="name: voice_assistant" [4]="name: oak_d_lite")
+    SYS_CTLS=([1]=ros_motor_control_node_boot.service [2]=ros_motor_current_node_boot.service [3]=pib_api_boot.service [4]=ros_camera_boot.service [5]=ros_cerebra_boot.service [6]=ros_voice_assistant_boot.service)
 
-for i in "${!FOLDERS[@]}"
-do
-    if [ -d "$ROS_WORKING_DIR_SRC/${FOLDERS[$i]}" ];then
-        if [[ ! $COLCON_INFO == *${PACKAGE_NAMES[$i]}* ]]; then
-            echo -e $red_text_color"The package ${FOLDERS[$i]} is not built"$reset_text_color
+    # Change directory to make the colcon command work independently of the location of this shell script
+    cd $ROS_WORKING_DIR_SRC
+    COLCON_INFO=$(colcon info)
+
+    for i in "${!FOLDERS[@]}"
+    do
+        if [ -d "$ROS_WORKING_DIR_SRC/${FOLDERS[$i]}" ];then
+            if [[ ! $COLCON_INFO == *${PACKAGE_NAMES[$i]}* ]]; then
+                echo -e $red_text_color"The package ${FOLDERS[$i]} is not built"$reset_text_color
+            else
+                echo -e ${FOLDERS[$i]} "package checked"
+            fi
         else
-            echo -e ${FOLDERS[$i]} "package checked"
+            echo -e $red_text_color"The package ${FOLDERS[$i]} is not installed"$reset_text_color
         fi
-    else
-        echo -e $red_text_color"The package ${FOLDERS[$i]} is not installed"$reset_text_color
-    fi
-done
+    done
 
-SERVICE_STATUS="Active: active (running)"
-for service_name in "${SYS_CTLS[@]}"
-do
-    if [[ ! $(systemctl status $service_name) == *$SERVICE_STATUS* ]]; then
-        echo -e $red_text_color"The service $service_name is not active"$reset_text_color
-    else 
-        echo -e $service_name "checked"
-    fi
-done
-echo -e $new_line"---pib packages and services checked---"
+    SERVICE_STATUS="Active: active (running)"
+    for service_name in "${SYS_CTLS[@]}"
+    do
+        if [[ ! $(systemctl status $service_name) == *$SERVICE_STATUS* ]]; then
+            echo -e $red_text_color"The service $service_name is not active"$reset_text_color
+        else 
+            echo -e $service_name "checked"
+        fi
+    done
+    echo -e $yellow_text_color"--- pib packages and services check completed ---"$reset_text_color
+fi
+
+echo -e $new_line$yellow_text_color"+++ system health check completed +++"$reset_text_color$new_line
 
 exit 0
