2a1f66a5abdb48fb13faf8b38793fb4d317f2d01
JaHe-isento <jan.hermann@isento.de>
2023-11-28 23:58:51 +0100

    feat(PR-368): Add console outputs

--- installation_files/get_setup_files.sh
+++ installation_files/get_setup_files.sh
@@ -2,6 +2,8 @@
 #
 # This script downloads the pib-setup repo and unzips it to a temporary directory
 
+echo -e "$YELLOW_TEXT_COLOR""-- Downloading setup files from github --""$RESET_TEXT_COLOR""$NEW_LINE"
+
 SETUP_ARCHIVE_LINK="https://github.com/pib-rocks/setup-pib/archive/refs/heads/""$SETUP_PIB_BRANCH"".zip"
 SETUP_ARCHIVE_NAME="setup-pib-""$SETUP_PIB_BRANCH"".zip"
 
@@ -11,3 +13,5 @@ mv "$SETUP_ARCHIVE_NAME" "$TEMPORARY_SETUP_DIR"
 unzip "$TEMPORARY_SETUP_DIR/""$SETUP_ARCHIVE_NAME" -d "$TEMPORARY_SETUP_DIR"
 
 INSTALLATION_FILES_DIR="$TEMPORARY_SETUP_DIR/""setup-pib-""$SETUP_PIB_BRANCH""/installation_files"
+
+echo -e "$NEW_LINE""$GREEN_TEXT_COLOR""-- Setup file download completed --""$RESET_TEXT_COLOR""$NEW_LINE"
