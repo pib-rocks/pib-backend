66af3ff8c416676e647d292f8042876a4a2d291c
Nico Hertel <nico.hertel@isento.de>
2023-12-13 13:38:55 +0100

    refactor(PR-446): simplify database path definition

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname("/home/pib/pib_data/"))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/pib/pib_data/pibdata.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
