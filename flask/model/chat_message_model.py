f91dd0ad03f472a96eebecad7cd2cfe19319b926
nhe <nico.hertel@isento.de>
2023-12-27 11:54:36 +0100

    feat(PR-389): chat messages are stored with proper datetime

--- flask/model/chat_message_model.py
+++ flask/model/chat_message_model.py
@@ -1,4 +1,5 @@
 from app.app import db
+from sqlalchemy.sql import func
 import uuid
 
 class ChatMessage(db.Model):
@@ -6,14 +7,13 @@ class ChatMessage(db.Model):
     __tablename__ = "chatMessage"
     id = db.Column(db.Integer, primary_key=True) 
     messageId = db.Column(db.String(255), nullable=False, unique=True) 
-    timestamp = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False , default=func.now())
     isUser = db.Column(db.Boolean, nullable=False) 
     content = db.Column(db.String(255), nullable=False)
     chatId = db.Column(db.String(255), db.ForeignKey('chat.chatId'), nullable=False)
     
     def __init__(self, isUser, content, chatId):
         self.messageId = str(uuid.uuid4())
-        self.timestamp = "TODO"
         self.isUser = isUser
         self.content = content
         self.chatId = chatId
