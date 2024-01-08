986358e8d12f7af2b0f7c98a4f34ef4bba4d3007
Vlad Stavila <vladislav.stavila@isento.de>
2024-01-08 08:56:25 +0100

    fix(PR-507): move tinkerforge download to temporary directory

--- installation_scripts/install_tinkerforge.sh
+++ installation_scripts/install_tinkerforge.sh
@@ -10,17 +10,17 @@ echo -e "$YELLOW_TEXT_COLOR""-- Installing TinkerForge --""$RESET_TEXT_COLOR"
 PLATFORM_TYPE=$(uname -m)
 if [ $PLATFORM_TYPE != 'aarch64' ]; then
 	echo "Installing Brick daemon for $PLATFORM_TYPE"
-	curl --location --output https://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_amd64.deb
-	sudo dpkg -i brickd_linux_latest_amd64.deb
+	curl --location https://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_amd64.deb --output  "$TEMPORARY_SETUP_DIR/brickd_linux_latest_amd64.deb"
+	sudo dpkg -i "$TEMPORARY_SETUP_DIR/brickd_linux_latest_amd64.deb"
 else
 	echo "Installing Brick daemon for $PLATFORM_TYPE"
-	curl --location --output https://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_arm64.deb
-	sudo dpkg -i brickd_linux_latest_arm64.deb
+	curl --location https://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_arm64.deb --output "$TEMPORARY_SETUP_DIR/brickd_linux_latest_arm64.deb"
+	sudo dpkg -i "$TEMPORARY_SETUP_DIR/brickd_linux_latest_arm64.deb"
 fi
 # Brick viewer
 sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtopengl python3-serial python3-tz python3-tzlocal
-curl --location --output https://download.tinkerforge.com/tools/brickv/linux/brickv_linux_latest.deb
-sudo dpkg -i brickv_linux_latest.deb
+curl --location https://download.tinkerforge.com/tools/brickv/linux/brickv_linux_latest.deb --output "$TEMPORARY_SETUP_DIR/brickv_linux_latest.deb"
+sudo dpkg -i "$TEMPORARY_SETUP_DIR/brickv_linux_latest.deb"
 # Tinkerforge python APIs
 curl --silent --location https://download.tinkerforge.com/apt/$(. /etc/os-release; echo $ID)/tinkerforge.gpg | sudo tee /etc/apt/trusted.gpg.d/tinkerforge.gpg > /dev/null
 echo "deb https://download.tinkerforge.com/apt/$(. /etc/os-release; echo $ID $VERSION_CODENAME) main" | sudo tee /etc/apt/sources.list.d/tinkerforge.list
