dd79d327529ad8edf2864274190d8017a15be4b8
Niklas Rothberger <niklas.rothberger@isento.de>
2024-01-10 08:07:32 +0100

    Fix URL path of API

--- flask/route/chat_route.py
+++ flask/route/chat_route.py
@@ -3,8 +3,8 @@ from controller import chat_controller
 
 blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/', methods=['POST'])(chat_controller.create_chat)
-blueprint.route('/', methods=['GET'])(chat_controller.get_all_chats)
+blueprint.route('', methods=['POST'])(chat_controller.create_chat)
+blueprint.route('', methods=['GET'])(chat_controller.get_all_chats)
 blueprint.route('/<string:uuid>', methods=['GET'])(chat_controller.get_chat_by_id)
 blueprint.route('/<string:uuid>', methods=['PUT'])(chat_controller.update_chat)
 blueprint.route('/<string:uuid>', methods=['DELETE'])(chat_controller.delete_chat)
