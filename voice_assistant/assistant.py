90bd9cb303532b4fbd24419d8be4ad24894c5b67
nhe <nico.hertel@isento.de>
2024-01-10 12:08:49 +0100

    feat(PR-389): persist and publish chat messages

--- voice_assistant/assistant.py
+++ voice_assistant/assistant.py
@@ -1,6 +1,12 @@
+from enum import Enum
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from rclpy.node import Node
+
+from datatypes.srv import SetVoiceAssistantState, GetVoiceAssistantState
+from datatypes.msg import ChatMessage, VoiceAssistantState
 
 import openai
 from google.cloud import texttospeech
@@ -12,50 +18,198 @@ import speech_recognition as sr
 from multiprocessing import Process, Value
 import time
 
+import json
+import ctypes
+from urllib import request as urllib_request, error as urllib_error
+
+
 # Set up OpenAI GPT-3 API
-openai.api_key = ""
+with open("/home/pib/ros_working_dir/src/voice-assistant/openapi-key") as f:
+    openai.api_key = f.read()
 
 # Google Cloud API
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/pib/ros_working_dir/src/voice-assistant/voice_assistant/pibVoice.json"
 client = speech.SpeechClient()
 
+WAITING_PERIOD_SECONDS = 0.5
+AUDIO_OUTPUT_FILE = "output.wav"
+CHAT_MESSAGE_ROUTE = "http://localhost:5000/voice-assistant/chat/%s/messages"
+
+
+
 class AssistantSubscriber(Node):
 
-    def __init__(self, start_bool, done_bool):
+    def __init__(self, start_bool, done_bool, chat_id):
+
         super().__init__('assistant_subscriber')
         self.get_logger().info('Now running VA')
-        self.subscription = self.create_subscription(String, '/cerebra_voice_settings', self.listener_callback, 10)
         self.start_bool = start_bool
         self.done_bool = done_bool
+        self.chat_id = chat_id
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        if msg.data == '{"activationFlag":true}':
-            print('Assistant activate')
-            self.start_bool.value = True
-        elif msg.data == '{"activationFlag":false}':
-            print('Assistant deactivate')
-            self.done_bool.value = True
-        else:
-            self.get_logger().warn('Strange message from cerebra')
+        chat_message_callback_group = MutuallyExclusiveCallbackGroup()
+        va_state_callback_group = MutuallyExclusiveCallbackGroup()
+        timer_callback_group = MutuallyExclusiveCallbackGroup()
 
-def start_listening(done_bool):
-    count = 1
-    while not done_bool.value:
-        time.sleep(0.2)
-        print("Say Something:")
-        user_input = speech_to_text()
-        gpt_response = gpt_chat(user_input)
-        print("Pib:", gpt_response)
-        output_file = "output.wav"
-        text_to_speech(gpt_response, output_file)
-        play_audio(output_file)
-        print("Pib is done speaking.")
-        os.remove(output_file)
-        print(str(count) + '-te Aussage seit Start.')
-        count += 1
+        # Service for setting VoiceAssistantState
+        self.set_voice_assistant_service = self.create_service(
+            SetVoiceAssistantState, 
+            'set_voice_assistant_state',
+            self.set_voice_assistant_state,
+            callback_group=va_state_callback_group
+        )
+
+        # Service for getting current VoiceAssistantState
+        self.get_voice_assistant_service = self.create_service(
+            GetVoiceAssistantState, 
+            'get_voice_assistant_state',
+            self.get_voice_assistant_state,
+            callback_group=va_state_callback_group
+        )
+
+        # Publisher for VoiceAssistantState
+        self.voice_assistant_state_publisher = self.create_publisher(
+            VoiceAssistantState, 
+            "voice_assistant_state", 
+            10,
+            callback_group=va_state_callback_group
+        )
+
+        # Publisher for ChatMessages
+        self.chat_message_publisher = self.create_publisher(
+            ChatMessage, 
+            "chat_messages", 
+            10,
+            callback_group=chat_message_callback_group
+        )
+
+        # Check for Start Signal periodically
+        self.timer = self.create_timer(
+            WAITING_PERIOD_SECONDS, 
+            self.timer_callback,
+            callback_group=timer_callback_group
+        )
+
+
+
+    def get_voice_assistant_state(self, request, response):
+
+        response.voice_assistant_state.chat_id = self.chat_id.value
+        response.voice_assistant_state.turned_on = self.start_bool.value
+        return response
+    
+
+
+    def set_voice_assistant_state(self, request, response):
+
+        response.successful = True
+        try:
+            if request.voice_assistant_state.turned_on: self.start_bool.value = True
+            else: self.done_bool.value = True 
+            self.chat_id.value = request.voice_assistant_state.chat_id
+            self.voice_assistant_state_publisher.publish(request.voice_assistant_state)
+        except Exception as e:
+            self.get_logger().info(f"following error occured while trying to set voice assistant state: {str(e)}.")
+            response.successful = False
+
+        return response
+
+
+
+    def timer_callback(self):
+
+        self.get_logger().info("Waiting to start...")
+        if not self.start_bool.value: return 0
+
+        self.get_logger().info("on")
+        while not self.done_bool.value:
+            current_chat_id = self.chat_id.value
+            input_received, user_input = self.get_user_input()
+            if not input_received: continue
+            self.persist_and_publish_message(user_input, True, current_chat_id)
+            response_received, va_response = self.get_va_response(user_input)
+            if not response_received: continue
+            self.persist_and_publish_message(va_response, False, current_chat_id)
+            self.play_audio(va_response)
+
+        self.get_logger().info("turning off voice-assistant")
+        self.start_bool.value = False
+        self.done_bool.value = False
+
+
+    
+    def get_user_input(self) -> (bool, str):
+
+        return True, speech_to_text()
+
+
+
+    def get_va_response(self, user_input) -> (bool, str):
+
+        return True, gpt_chat(user_input)
+
+
+
+    def play_audio(self, text):
+
+        text_to_speech(text, AUDIO_OUTPUT_FILE)
+        play_audio(AUDIO_OUTPUT_FILE)
+    
+
+
+    def persist_and_publish_message(self, message_content: str, is_user: bool, chat_id: str):
+
+        try:
+                # compile motor settings to UTF-8 encoded JSON string
+                request_body = json.dumps({
+                    'content': message_content,
+                    'isUser': is_user
+                }).encode('UTF-8'),
+                
+                # create 'PUT' request to '/motor-settings'
+                request = urllib_request.Request(
+                        CHAT_MESSAGE_ROUTE % chat_id,
+                        method='POST',
+                        data=request_body,
+                        headers={ "Content-Type": "application/json" }                           
+                )
+
+                # send request to pib-api and publish response to topic
+                with urllib_request.urlopen(request) as response:
+                        
+                        response_json = json.loads(response.read().decode('utf-8'))
+                        chat_message = ChatMessage()
+                        chat_message.chat_id = chat_id
+                        chat_message.content = message_content
+                        chat_message.is_user = is_user
+                        chat_message.message_id = response_json['messageId']
+                        chat_message.timestamp = response_json['timestamp']
+                        self.chat_message_publisher.publish(chat_message)
+                        
+                return True
+                
+        except urllib_error.HTTPError as e: # if server response has 4XX or 5XX status code   
+                self.get_logger().error(
+                        f"Error sending HTTP-Request, received following response from server: \n" +
+                        f"\tstatus: {e.code},\n" +
+                        f"\treason: {e.reason},\n" +
+                        f"\tresponse-body: {e.fp.read().decode()}" +
+                        f"Request that caused the error:\n" +
+                        f"\turl: {request.full_url},\n" +
+                        f"\tmethod: {request.method},\n" +
+                        f"\tbody: {request.data},\n" +
+                        f"\theaders: {request.header_items()}"
+                )
+        
+        except Exception as e: # if something else fails
+                self.get_logger().warn(f"Error while sending HTTP-Request: {str(e)}")
+
+        return False
+
+
 
 def speech_to_text():
+
     r = sr.Recognizer()
     print('----------------------------------------------ALSA')
     with sr.Microphone() as source:
@@ -74,7 +228,11 @@ def speech_to_text():
         print('Request error from Google Speech Recognition')
     return data
 
+
+
+
 def gpt_chat(prompt):
+
     response = openai.ChatCompletion.create(
         model="gpt-4-0314",
         messages=[
@@ -90,7 +248,11 @@ def gpt_chat(prompt):
     )
     return response['choices'][0]['message']['content']
 
+
+
+
 def text_to_speech(text, output_file):
+
     client = texttospeech.TextToSpeechClient()
     synthesis_input = texttospeech.SynthesisInput(text=text)
     voice = texttospeech.VoiceSelectionParams(
@@ -106,7 +268,10 @@ def text_to_speech(text, output_file):
     with open(output_file, "wb") as out:
         out.write(response.audio_content)
 
+
+
 def play_audio(file_path):
+
     CHUNK = 1024
     wf = wave.open(file_path, 'rb')
     print(file_path)
@@ -130,36 +295,23 @@ def play_audio(file_path):
     stream.close()
     p.terminate()
 
-def toggler(start_bool, done_bool):
-    rclpy.init()
-    subscriber_node = AssistantSubscriber(start_bool, done_bool)
-    rclpy.spin(subscriber_node)
-    subscriber_node.destroy_node()
-    rclpy.shutdown()
+
 
 def main(args=None):
-    start_bool = Value('b', False)
-    done_bool = Value('b', False)
 
-    print('toggler started')
-    toggle_process = Process(target=toggler, args=(start_bool, done_bool))
-    toggle_process.start()
-    while True:
-        start_bool.value = False
-        print('Waiting to start.')
-        while not start_bool.value:
-            time.sleep(0.5)
+    start_bool = Value(ctypes.c_bool, False)
+    done_bool = Value(ctypes.c_bool, False)
+    chat_id = Value(ctypes.c_wchar_p, "")
+    
+    rclpy.init()
+    node = AssistantSubscriber(start_bool, done_bool, chat_id)
+    executor = MultiThreadedExecutor(4)
+    executor.add_node(node)
+    executor.spin()
+    node.destroy_node()
+    rclpy.shutdown()
 
-        done_bool.value = False
-        print('on')
-        assistant_process = Process(target=start_listening, args=(done_bool,))
-        assistant_process.start()
 
-        print('Press off when done.')
-        while not done_bool.value:
-            time.sleep(0.5)
-
-        assistant_process.terminate()
 
 if __name__ == "__main__":
     main()
