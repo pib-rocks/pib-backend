1494c02ceac929b2b1077d3461ba6c722a216e0a
nhe <nico.hertel@isento.de>
2024-01-18 13:14:10 +0100

    feat(PR-508): enable foreign key constraints upon connecting with database

--- flask/run.py
+++ flask/run.py
@@ -1,7 +1,8 @@
-from app.app import app
+from app.app import app, db
 from route import program_route
 from route import program_route, chat_route, motor_route, personality_route, camera_route
 from controller import error_handler;
+from sqlalchemy import event
 
 app.register_blueprint(program_route.blueprint, url_prefix='/program', name='program')
 app.register_blueprint(chat_route.blueprint, url_prefix='/voice-assistant/chat', name='chat')
@@ -13,6 +14,10 @@ app.register_error_handler(404, error_handler.handle_not_found_error)
 app.register_error_handler(500, error_handler.handle_internal_server_error)
 app.register_error_handler(501, error_handler.handle_not_implemented_error)
 
+def on_connect(dbapi_con, con_record):
+    dbapi_con.execute('PRAGMA FOREIGN_KEYS=ON')
+
 if __name__ == '__main__':
+    with app.app_context(): event.listen(db.engine, 'connect', on_connect)
     app.run(host="0.0.0.0", port=5000)
 
