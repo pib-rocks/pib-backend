5ccc2b0dc6a8b3d68d4207314a69614905ac6898
nhe <nico.hertel@isento.de>
2024-01-22 09:02:49 +0100

    feat(PR-508): add try-except block in return statements for motor- and bricklet controllers

--- flask/controller/bricklet_controller.py
+++ flask/controller/bricklet_controller.py
@@ -1,8 +1,10 @@
 from schema.bricklet_schema import bricklets_uid_only_schema
 from service import bricklet_service
 from app.app import db
-from flask import jsonify, request
+from flask import jsonify, request, abort
+
 
 def get_all_bricklets():
     bricklets = bricklet_service.get_all_bricklets()
-    return jsonify({"bricklets": bricklets_uid_only_schema.dump(bricklets)})
+    try: return jsonify({"bricklets": bricklets_uid_only_schema.dump(bricklets)})
+    except: abort(500)
--- flask/controller/motor_controller.py
+++ flask/controller/motor_controller.py
@@ -1,17 +1,19 @@
 from schema.motor_schema import motor_schema, motors_schema, motor_settings_schema, motor_bricklet_pins_schema
 from service import motor_service
 from app.app import db
-from flask import request, jsonify
+from flask import request, jsonify, abort
 
 
 def get_all_motors():
     motors = motor_service.get_all_motors()
-    return jsonify({'motors': motors_schema.dump(motors)})
+    try: return jsonify({'motors': motors_schema.dump(motors)})
+    except: abort(500)
 
 
 def get_motor(name: str):
     motor = motor_service.get_motor_by_name(name)
-    return motor_schema.dump(motor)
+    try: return motor_schema.dump(motor)
+    except: abort(500)
 
 
 def update_motor(name: str):
@@ -20,28 +22,33 @@ def update_motor(name: str):
     motor_service.set_bricklet_pins(name, bricklet_pin_dtos)
     motor = motor_service.set_motor_settings(name, motor_settings_dto)
     db.session.commit()
-    return motor_schema.dump(motor)
+    try: return motor_schema.dump(motor)
+    except: abort(500)
 
 
 def get_motor_settings(name: str):
     motor = motor_service.get_motor_by_name(name)
-    return motor_settings_schema.dump(motor)
+    try: return motor_settings_schema.dump(motor)
+    except: abort(500)
 
 
 def update_motor_settings(name: str):
     motor_settings_dto = motor_settings_schema.load(request.json)
     motor = motor_service.set_motor_settings(name, motor_settings_dto)
     db.session.commit()
-    return motor_settings_schema.dump(motor)
+    try: return motor_settings_schema.dump(motor)
+    except: abort(500)
 
 
 def get_motor_bricklet_pins(name: str):
     motor = motor_service.get_motor_by_name(name)
-    return motor_bricklet_pins_schema.dump(motor)
+    try: return motor_bricklet_pins_schema.dump(motor)
+    except: abort(500)
 
 
 def update_motor_bricklet_pins(name: str):
     bricklet_pin_dtos = motor_bricklet_pins_schema.load(request.json)['brickletPins']
     motor = motor_service.set_bricklet_pins(name, bricklet_pin_dtos)
     db.session.commit()
-    return motor_bricklet_pins_schema.dump(motor)
+    try: return motor_bricklet_pins_schema.dump(motor)
+    except: abort(500)
