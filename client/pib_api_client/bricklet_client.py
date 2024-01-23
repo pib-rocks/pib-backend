81fdddf25d05210af07843ceebc5c3bcdaa26909
nhe <nico.hertel@isento.de>
2024-01-23 07:53:28 +0100

    feat(PR-508): create client for motor and bricklet

--- client/pib_api_client/bricklet_client.py
+++ client/pib_api_client/bricklet_client.py
@@ -3,19 +3,9 @@ from typing import Any
 from pib_api_client import send_request, URL_PREFIX
 
 
-GET_ALL_MOTORS_URL = URL_PREFIX + '/motor'
-UPDATE_MOTOR_SETTINGS_URL = URL_PREFIX + '/motor/%s/settings'
+GET_ALL_BRICKLETS_URL = URL_PREFIX + '/bricklet'
 
 
-def get_all_motors() -> (bool, dict[str, Any]):
-        request = Request(GET_ALL_MOTORS_URL, method='GET')
-        return send_request(request)
-
-
-def update_motor_settings(motor_name, motor_settings_dto) -> (bool, dict[str, Any]):
-        request = Request(
-                UPDATE_MOTOR_SETTINGS_URL % motor_name, 
-                method='PUT',
-                headers = { 'Content-Type': 'application/json' },
-                body = json.dumps(motor_settings_dto).encode('UTF-8'))
+def get_all_bricklets() -> (bool, dict[str, Any]):
+        request = Request(GET_ALL_BRICKLETS_URL, method='GET')
         return send_request(request)
