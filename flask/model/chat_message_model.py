381d504f3cde0d1ab0c950c98be1bdd125c7f581
Nico Hertel <nico.hertel@isento.de>
2024-01-24 15:49:41 +0100

    refactor(PR-389): chat-controller

--- flask/model/chat_message_model.py
+++ flask/model/chat_message_model.py
@@ -5,15 +5,10 @@ import uuid
 class ChatMessage(db.Model):
 
     __tablename__ = "chatMessage"
+    
     id = db.Column(db.Integer, primary_key=True) 
     messageId = db.Column(db.String(255), nullable=False, unique=True) 
     timestamp = db.Column(db.DateTime, nullable=False , default=func.now())
     isUser = db.Column(db.Boolean, nullable=False) 
     content = db.Column(db.String(100000), nullable=False)
     chatId = db.Column(db.String(255), db.ForeignKey('chat.chatId'), nullable=False)
-    
-    def __init__(self, isUser, content, chatId):
-        self.messageId = str(uuid.uuid4())
-        self.isUser = isUser
-        self.content = content
-        self.chatId = chatId
--- flask/schema/chat_schema.py
+++ flask/schema/chat_schema.py
@@ -1,5 +1,6 @@
 from model.chat_model import Chat
 from app.app import ma
+from schema.chat_message_schema import chat_messages_schema
 from marshmallow import fields
 
 
@@ -8,7 +9,9 @@ class ChatSchema(ma.SQLAlchemyAutoSchema):
         model = Chat
         exclude = ('id',)
     personalityId = fields.String()
+    messages = fields.Nested(chat_messages_schema)
 
-chat_schema = ChatSchema()
-chats_schema = ChatSchema(many=True)
+chat_schema = ChatSchema(exclude=('messages',))
+chats_schema = ChatSchema(many=True, exclude=('messages',))
 upload_chat_schema = ChatSchema(only=['topic', 'personalityId'])
+chat_messages_only_schema = ChatSchema(only=('messages',))
