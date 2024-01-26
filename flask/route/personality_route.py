057e48ce7d5b0142f3d7f2fd68fed6b1be230b2c
nhe <nico.hertel@isento.de>
2024-01-26 14:38:38 +0100

    fix(PR-563): behavior when updating personality and no description is provided

--- flask/route/personality_route.py
+++ flask/route/personality_route.py
@@ -4,7 +4,7 @@ from controller import personality_controller
 blueprint = Blueprint('blueprint', __name__)
 
 blueprint.route('', methods=['GET'])(personality_controller.get_all_personalities)
-blueprint.route('/<string:uuid>', methods=['GET'])(personality_controller.get_personality_by_id)
 blueprint.route('', methods=['POST'])(personality_controller.create_personality)
-blueprint.route('/<string:uuid>', methods=['PUT'])(personality_controller.update_personality)
-blueprint.route('/<string:uuid>', methods=['DELETE'])(personality_controller.delete_personality)
+blueprint.route('/<string:personality_id>', methods=['GET'])(personality_controller.get_personality)
+blueprint.route('/<string:personality_id>', methods=['PUT'])(personality_controller.update_personality)
+blueprint.route('/<string:personality_id>', methods=['DELETE'])(personality_controller.delete_personality)
