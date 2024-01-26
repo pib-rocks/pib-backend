65b53766ab403a363d42b53e27206424b1c87ed4
Niklas Rothberger <niklas.rothberger@isento.de>
2024-01-26 16:37:54 +0100

    feat(PR-521): Add update bricklert add to client

--- client/pib_api_client/bricklet_client.py
+++ client/pib_api_client/bricklet_client.py
@@ -1,11 +1,30 @@
 from urllib.request import Request
 from typing import Any
 from pib_api_client import send_request, URL_PREFIX
+from  tinkerforge_service import TinkerForge
+
 
 
 GET_ALL_BRICKLETS_URL = URL_PREFIX + '/bricklet'
+tinkerforge_service = TinkerForge()
 
 
 def get_all_bricklets() -> (bool, dict[str, Any]):
         request = Request(GET_ALL_BRICKLETS_URL, method='GET')
         return send_request(request)
+
+def update_all_bricklets_on_start() ->():
+        tinkerforge_service.get_ids()
+        Request(GET_ALL_BRICKLETS_URL+"1", {{"uid": tinkerforge_service.UID0}}, method='PUT')
+        Request(GET_ALL_BRICKLETS_URL+"2", {{"uid": tinkerforge_service.UID1}}, method='PUT')
+        Request(GET_ALL_BRICKLETS_URL+"3", {{"uid": tinkerforge_service.UID2}}, method='PUT')
+        return
+    
+def detact_changes_between_db_and_tinkerforge() ->():
+        usedUIDs = get_all_bricklets()
+        print(usedUIDs)
+        #tinker.get_ids()
+        for i in range(len(usedUIDs)):
+                if(usedUIDs[i] != tinkerforge_service.getValue(i)):
+                        return True
+        return False
