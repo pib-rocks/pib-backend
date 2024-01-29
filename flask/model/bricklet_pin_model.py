df780eb5b729902c4daec152eb87356408ff7b8f
Nico Hertel <nico.hertel@isento.de>
2024-01-29 15:53:55 +0100

    fix(develop): bricklet-model

--- flask/model/bricklet_pin_model.py
+++ flask/model/bricklet_pin_model.py
@@ -1,8 +1,6 @@
 from app.app import db
 from model.bricklet_model import Bricklet
 
-from sqlalchemy.ext.associationproxy import association_proxy
-
 class BrickletPin(db.Model):
 
     __tablename__ = "brickletPin"
@@ -11,7 +9,3 @@ class BrickletPin(db.Model):
     motorId = db.Column(db.Integer, db.ForeignKey('motor.id'), nullable=False)
     brickletId = db.Column(db.Integer, db.ForeignKey('bricklet.id'), nullable=False)
     pin = db.Column(db.Integer, nullable=False)
-
-    # def __init__(self, pin, brickletId):
-    #     self.pin = pin
-    #     self.brickletId = brickletId
