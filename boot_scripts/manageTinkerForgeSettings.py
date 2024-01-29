09fcf19ff896a968dbb017d145245a629cf10cca
Niklas Rothberger <niklas.rothberger@isento.de>
2024-01-29 10:24:22 +0100

    feat(PR-521): motor controller & current using python script, to geht uid

--- boot_scripts/manageTinkerForgeSettings.py
+++ boot_scripts/manageTinkerForgeSettings.py
@@ -10,7 +10,7 @@ class TinkerForge:
         UID1 = "Y"
         UID2 = "Z"
 
-        def getIDs(self):
+        def ge_ids(self):
             self.ipcon.register_callback(IPConnection.CALLBACK_ENUMERATE, self.cb_enumerate)
             self.ipcon.enumerate()
             self.printAllUIDs()
@@ -24,11 +24,11 @@ class TinkerForge:
             if position == "e":
                     self.UID2 = uid
 
-        def getValue(cls, number):
+        def get_value(cls, number):
             variable_name = "UID" + str(number)
             return getattr(cls, variable_name, None)
         
-        def printAllUIDs(self):
+        def print_all_uids(self):
                 print("UID0 = " + self.UID0)
                 print("UID1 = " + self.UID1)
                 print("UID2 = " + self.UID2)
@@ -41,23 +41,23 @@ class TinkerForge:
         
 tinker = TinkerForge()
 
-def updateUIDs():
-    tinker.getIDs()
+def update_uids():
+    tinker.ge_ids()
     header = {"Content-Type": "application/json"}
     response0 = requests.put("http://127.0.0.1:5000//bricklet/1", data=json.dumps({"uid": tinker.UID0}), headers=header)
     response1 = requests.put("http://127.0.0.1:5000//bricklet/2", data=json.dumps({"uid": tinker.UID1}), headers=header)
     response2 = requests.put("http://127.0.0.1:5000//bricklet/3", data=json.dumps({"uid": tinker.UID2}), headers=header)
     return
 
-def getUIDsFromDB():
+def get_uids_from_db():
       response = requests.get("http://127.0.0.1:5000//bricklet")
       json_data = json.loads(response.text)
       return [json_data['bricklets'][0]['uid'], json_data['bricklets'][1]['uid'], json_data['bricklets'][2]['uid']]
     
 
-def changesBetweenCurrentAndTinkerForgeUIDs():
-      usedUIDs = getUIDsFromDB()
-      tinker.getIDs()
+def changes_between_current_and_tinker_forge_uids():
+      usedUIDs = get_uids_from_db()
+      tinker.ge_ids()
       for i in range(len(usedUIDs)):
             if(usedUIDs[i] != tinker.getValue(i)):
                 return True
