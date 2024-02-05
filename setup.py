6416cf67af5eeb085aabedb0ff5dc8f8f73ff10b
nhe <nico.hertel@isento.de>
2024-02-05 09:12:01 +0100

    feat(PR-564): add program-node and proxy-program-node for running user-programs

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'program'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pib',
+    maintainer_email='pib@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'program = program.program:main',
+            'proxy_program = program.proxy_program:main'
+        ],
+    },
+)
