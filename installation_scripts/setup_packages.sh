04fa081991c66164fb169c8a53be6ca40dd9ce48
JaHe-isento <jan.hermann@isento.de>
2024-02-08 14:59:09 +0100

    feat(PR-521): Add service for bricklet uid script

--- installation_scripts/setup_packages.sh
+++ installation_scripts/setup_packages.sh
@@ -32,7 +32,7 @@ pip install -r requirements.txt
 #
 #check on git
 echo 'check if git init is done'
-cd $ROS_WORKING_DIR/src
+cd "$ROS_WORKING_DIR""/src"
 if [ ! -f .git ]; then
 	git init
 fi
@@ -43,11 +43,7 @@ git pull https://github.com/pib-rocks/ros-packages.git
 # Run the script for creating a custom gitmodules file
 readonly CREATE_GITMODULE_FILE_SCRIPT="$installation_files_dir"/"create_gitmodule_file.sh"
 chmod 755 "$CREATE_GITMODULE_FILE_SCRIPT"
-if [ "$is_dev_mode" = "$TRUE"]; then
-	source "$CREATE_GITMODULE_FILE_SCRIPT" "-d" "$user_default_branch" "$user_feature_branch"
-else
-	source "$CREATE_GITMODULE_FILE_SCRIPT"
-fi
+source "$CREATE_GITMODULE_FILE_SCRIPT"
 
 # Pull all repo branches as specified in the gitmodules file
 git submodule init
@@ -62,31 +58,38 @@ touch "$VOICE_ASSISTANT_CREDENTIALS_DIR""/google-key"
 
 echo "Booting all nodes..."
 # Boot camera
-sudo chmod 755 $ROS_CAMERA_BOOT_DIR/ros_camera_boot.sh
-sudo chmod 755 $ROS_CAMERA_BOOT_DIR/ros_camera_boot.service
-sudo mv $ROS_CAMERA_BOOT_DIR/ros_camera_boot.service /etc/systemd/system
+sudo chmod 755 "$ROS_CAMERA_BOOT_DIR""/ros_camera_boot.sh"
+sudo chmod 755 "$ROS_CAMERA_BOOT_DIR""/ros_camera_boot.service"
+sudo mv "$ROS_CAMERA_BOOT_DIR""/ros_camera_boot.service" /etc/systemd/system
 sudo systemctl enable ros_camera_boot.service
 
+# Boot bricklet uid script
+pip install "$ROS_WORKING_DIR""/src/motors/utils"
+sudo chmod 755 "$ROS_WORKING_DIR""/src/motors/utils/update_bricklet_uids.py"
+sudo chmod 755 "$ROS_MOTORS_BOOT_DIR""/bricklet_uid_boot.service"
+sudo mv "$ROS_MOTORS_BOOT_DIR""/bricklet_uid_boot.service" /etc/systemd/system
+sudo systemctl enable bricklet_uid_boot.service
+
 # Boot motor control node
-pip install "$ROS_WORKING_DIR/src/motors/pib_motors"
-sudo chmod 755 $ROS_MOTORS_BOOT_DIR/ros_motor_control_node_boot.sh
-sudo chmod 755 $ROS_MOTORS_BOOT_DIR/ros_motor_control_node_boot.service
-sudo mv $ROS_MOTORS_BOOT_DIR/ros_motor_control_node_boot.service /etc/systemd/system
+pip install "$ROS_WORKING_DIR""/src/motors/pib_motors"
+sudo chmod 755 "$ROS_MOTORS_BOOT_DIR""/ros_motor_control_node_boot.sh"
+sudo chmod 755 "$ROS_MOTORS_BOOT_DIR""/ros_motor_control_node_boot.service"
+sudo mv "$ROS_MOTORS_BOOT_DIR""/ros_motor_control_node_boot.service" /etc/systemd/system
 sudo systemctl enable ros_motor_control_node_boot.service
 
 # Boot motor current node
-sudo chmod 755 $ROS_MOTORS_BOOT_DIR/ros_motor_current_node_boot.sh
-sudo chmod 755 $ROS_MOTORS_BOOT_DIR/ros_motor_current_node_boot.service
-sudo mv $ROS_MOTORS_BOOT_DIR/ros_motor_current_node_boot.service /etc/systemd/system
+sudo chmod 755 "$ROS_MOTORS_BOOT_DIR""/ros_motor_current_node_boot.sh"
+sudo chmod 755 "$ROS_MOTORS_BOOT_DIR""/ros_motor_current_node_boot.service"
+sudo mv "$ROS_MOTORS_BOOT_DIR""/ros_motor_current_node_boot.service" /etc/systemd/system
 sudo systemctl enable ros_motor_current_node_boot.service
 
 # Boot voice-assistant
-sudo chmod 755 $ROS_VOICE_ASSISTANT_BOOT_DIR/ros_voice_assistant_boot.sh
-sudo chmod 755 $ROS_VOICE_ASSISTANT_BOOT_DIR/ros_voice_assistant_boot.service
-sudo mv $ROS_VOICE_ASSISTANT_BOOT_DIR/ros_voice_assistant_boot.service /etc/systemd/system
+sudo chmod 755 "$ROS_VOICE_ASSISTANT_BOOT_DIR""/ros_voice_assistant_boot.sh"
+sudo chmod 755 "$ROS_VOICE_ASSISTANT_BOOT_DIR""/ros_voice_assistant_boot.service"
+sudo mv "$ROS_VOICE_ASSISTANT_BOOT_DIR""/ros_voice_assistant_boot.service" /etc/systemd/system
 sudo systemctl enable ros_voice_assistant_boot.service
 
-cd $ROS_WORKING_DIR
+cd "$ROS_WORKING_DIR"
 colcon build
 
 echo -e "$NEW_LINE""$GREEN_TEXT_COLOR""-- Custom ros package setup completed --""$RESET_TEXT_COLOR""$NEW_LINE"
