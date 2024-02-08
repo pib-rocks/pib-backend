473884eee67155f66261ef9f83d87a1cf9dadc55
JaHe-isento <jan.hermann@isento.de>
2024-02-08 18:06:01 +0100

    feat(PR-521): Change custom package installation process

--- installation_scripts/setup_packages.sh
+++ installation_scripts/setup_packages.sh
@@ -30,18 +30,17 @@ git clone https://github.com/geaxgx/depthai_hand_tracker.git
 cd depthai_hand_tracker
 pip install -r requirements.txt
 #
-#check on git
-echo 'check if git init is done'
+# clone ros-packages-repo
+echo 'git clone packages with sub modules'
+cd $ROS_WORKING_DIR
+git clone https://github.com/pib-rocks/ros-packages.git
+mv "$ROS_WORKING_DIR""/ros-packages" "$ROS_WORKING_DIR""/src"
 cd "$ROS_WORKING_DIR""/src"
-if [ ! -f .git ]; then
-	git init
-fi
-#git pull packages with sub modules
-echo 'git pull packages with sub modules'
-git pull https://github.com/pib-rocks/ros-packages.git
+git checkout ${repo_map["$ROS_PACKAGES_ORIGIN"]}
+sudo chmod -R 777 "$ROS_WORKING_DIR"
 
 # Run the script for creating a custom gitmodules file
-readonly CREATE_GITMODULE_FILE_SCRIPT="$installation_files_dir"/"create_gitmodule_file.sh"
+readonly CREATE_GITMODULE_FILE_SCRIPT="$installation_files_dir""/create_gitmodule_file.sh"
 chmod 755 "$CREATE_GITMODULE_FILE_SCRIPT"
 source "$CREATE_GITMODULE_FILE_SCRIPT"
 
