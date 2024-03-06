5c8bbb73f8333ddeb005fe5640667ad390862e67
nhe <nico.hertel@isento.de>
2024-03-06 11:35:23 +0100

    fix(PR-600): text-to-speech boot-script

--- ros_packages/voice_assistant/boot_scripts/ros_text_to_speech_boot.sh
+++ ros_packages/voice_assistant/boot_scripts/ros_text_to_speech_boot.sh
@@ -2,4 +2,4 @@
 source /home/pib/ros_working_dir/ros_config.sh
 source /opt/ros/humble/setup.bash
 source /home/pib/ros_working_dir/install/setup.bash
-ros2 run voice_assistant text_to_speech.py
+ros2 run voice_assistant text_to_speech
