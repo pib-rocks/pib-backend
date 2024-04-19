995b693209b2f5a4ede42e7653f20b12fb27ced7
JaHe-isento <jan.hermann@isento.de>
2024-04-19 19:01:15 +0200

    feat(PR-615): Handle positions outside of rotation range
    We didn't do this until now because we misinterpreted a tinkerforge function

--- ros_packages/motors/pib_motors/pib_motors/motor.py
+++ ros_packages/motors/pib_motors/pib_motors/motor.py
@@ -5,11 +5,13 @@ from pib_api_client import motor_client
 
 class Motor:
 
-	def __init__(self, name: str, bricklet_pins: list[BrickletPin], invert: bool):
+	def __init__(self, name: str, bricklet_pins: list[BrickletPin], invert: bool, rotation_range_min: int, rotation_range_max: int):
 		self.name: str = name
 		self.visible: bool = True
 		self.bricklet_pins = bricklet_pins
 		self.invert: bool = invert
+		self.rotation_range_min: int = rotation_range_min
+		self.rotation_range_max: int = rotation_range_max
 
 	def __str__(self):
 		return f"MOTOR[ bricklet_pins: {[str(bp) for bp in self.bricklet_pins]}, settings: {self.get_settings()} ]"
@@ -17,6 +19,14 @@ class Motor:
 	def apply_settings(self, settings_dto) -> bool:
 		self.visible = settings_dto['visible']
 		self.invert = settings_dto['invert']
+		self.rotation_range_min = settings_dto['rotationRangeMin']
+		self.rotation_range_max = settings_dto['rotationRangeMax']
+
+		# Check if current position is outside of new rotation Ranges
+		adjusted_position = self.validate_position(self.get_position())
+		if adjusted_position != self.get_position():
+			self.set_position(adjusted_position)
+		
 		return all(bp.apply_settings(settings_dto) for bp in self.bricklet_pins)
 
 	def get_settings(self) -> dict[str, Any]:
@@ -24,11 +34,14 @@ class Motor:
 		motor_settings_dto['visible'] = self.visible
 		motor_settings_dto['name'] = self.name
 		motor_settings_dto['invert'] = self.invert
+		motor_settings_dto['rotationRangeMin'] = self.rotation_range_min
+		motor_settings_dto['rotationRangeMax'] = self.rotation_range_max
 		return motor_settings_dto
 
 	def set_position(self, position: int) -> bool:
 		if self.invert:
 			position = position * -1
+		position = self.validate_position(position)
 		return all(bp.set_position(position, bp.invert) for bp in self.bricklet_pins)
 	
 	def get_position(self) -> int:
@@ -41,6 +54,11 @@ class Motor:
 				return True
 		return False
 
+	def validate_position(self, position: int) -> int:
+		"""Check if position is within range, set it to te min/max value if not."""
+		position = min(max(position, self.rotation_range_min), self.rotation_range_max)
+		return position
+
 # get data from pib-api
 successful, response = motor_client.get_all_motors()
 if not successful: raise RuntimeError('failed to load motors from pib-api...')
@@ -50,7 +68,7 @@ motors: list[Motor] = []
 for motor_dto in response['motors']:
 
 	bricklet_pins = [BrickletPin(bricklet_pin_dto['pin'], bricklet_pin_dto['bricklet'], bricklet_pin_dto['invert']) for bricklet_pin_dto in motor_dto['brickletPins']]
-	motors.append(Motor(motor_dto['name'], bricklet_pins, motor_dto['invert']))
+	motors.append(Motor(motor_dto['name'], bricklet_pins, motor_dto['invert'], motor_dto['rotationRangeMin'], motor_dto['rotationRangeMax']))
 
 # maps the name of a (multi-)motor to its associated motor objects
 name_to_motors: dict[str, Motor] = { motor.name : [motor] for motor in motors }
