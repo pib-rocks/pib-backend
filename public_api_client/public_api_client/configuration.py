b0fa58f01431f9029b156ce4e3e736b0c5df3087
nhe <nico.hertel@isento.de>
2024-04-19 12:11:35 +0200

    feat(PR-532): use environment variable for publica-pi-config file path

--- public_api_client/public_api_client/configuration.py
+++ public_api_client/public_api_client/configuration.py
@@ -1,6 +1,8 @@
 import json
+import os
 
-with open("/home/pib/public_api/config.json") as f: _data = f.read()
+config_file_path = os.getenv("PUBLIC_API_CONFIG_FILE_PATH", "/home/pib/public_api/config.json")
+with open(config_file_path) as f: _data = f.read()
 _config = json.loads(_data)
 
 tryb_url_prefix = _config["trybUrlPrefix"]
