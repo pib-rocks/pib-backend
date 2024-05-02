81d2932906b51ac558a30594dee5bcae0f0694f2
Nino Meisinger <nino.meisinger@isento.de>
2024-05-02 10:18:33 +0200

    fix(PR-635): Rename Assistant Schema

--- pib_api/flask/schema/assistant_model_schema.py
+++ pib_api/flask/schema/assistant_model_schema.py
@@ -2,10 +2,10 @@ from model.assistant_model import AssistantModel
 from schema.sql_auto_with_camel_case_schema import SQLAutoWithCamelCaseSchema
 
 
-class AssistantSchemaSQLAutoWith(SQLAutoWithCamelCaseSchema):
+class AssistantModelSchemaSQLAutoWith(SQLAutoWithCamelCaseSchema):
     class Meta:
         model = AssistantModel
 
 
-assistant_model_schema = AssistantSchemaSQLAutoWith()
-assistant_models_schema = AssistantSchemaSQLAutoWith(many=True)
+assistant_model_schema = AssistantModelSchemaSQLAutoWith()
+assistant_models_schema = AssistantModelSchemaSQLAutoWith(many=True)
