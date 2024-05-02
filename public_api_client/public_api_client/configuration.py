895c58b1bf1afb79c6f686e31219db929df51e9f
Nico Hertel <nico.hertel@isento.de>
2024-05-02 08:22:58 +0200

    feat(PR-532): read public-api configuration from env-variables, if file-path to config.json does not exist

--- public_api_client/public_api_client/configuration.py
+++ public_api_client/public_api_client/configuration.py
@@ -1,9 +1,13 @@
 import json
 import os
 
-config_file_path = os.getenv("PUBLIC_API_CONFIG_FILE_PATH", "/home/pib/public_api/config.json")
-with open(config_file_path) as f: _data = f.read()
-_config = json.loads(_data)
+try:
+    with open("/home/pib/public_api/config.json") as f: _data = f.read()
+    _config = json.loads(_data)
+    tryb_url_prefix = _config["trybUrlPrefix"]
+    public_api_token = _config["publicApiToken"]
+    
+except FileNotFoundError:
+    tryb_url_prefix = os.getenv("TRYB_URL_PREFIX")
+    public_api_token = os.getenv("PUBLIC_API_TOKEN")
 
-tryb_url_prefix = _config["trybUrlPrefix"]
-public_api_token = _config["publicApiToken"]
