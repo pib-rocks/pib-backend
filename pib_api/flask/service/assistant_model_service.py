a950be89cd25a0e0aa44ed54fb30a510ef71e1d1
Nino Meisinger <nino.meisinger@isento.de>
2024-05-02 10:48:45 +0200

    refactor(PR-635): Rename assistant_service

--- pib_api/flask/service/assistant_model_service.py
+++ pib_api/flask/service/assistant_model_service.py
@@ -3,9 +3,9 @@ from model.assistant_model import AssistantModel
 from app.app import db
 
 
-def get_all_assistants() -> List[AssistantModel]:
+def get_all_assistant_models() -> List[AssistantModel]:
     return AssistantModel.query.all()
 
 
-def get_assistant_by_id(assistant_model_id: int) -> AssistantModel:
+def get_assistant_model_by_id(assistant_model_id: int) -> AssistantModel:
     return AssistantModel.query.filter_by(id=assistant_model_id).first()
