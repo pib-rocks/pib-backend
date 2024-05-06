57876fc5ec4e9c379a71091ae839496e8c787d2e
Nino Meisinger <nino.meisinger@isento.de>
2024-05-06 08:54:20 +0200

    fix(PR-635): fix assistantModel DTO creation

--- pib_api/client/pib_api_client/voice_assistant_client.py
+++ pib_api/client/pib_api_client/voice_assistant_client.py
@@ -24,7 +24,7 @@ class Personality:
         self.language = "German"  # TODO: language should be stored as part of a personality -> personality_dto["language"]
         self.pause_threshold = personality_dto["pauseThreshold"]
         self.description = personality_dto.get("description")
-        self.assistant_model = self._get_assistant_model(personality_dto["assistantId"])
+        self.assistant_model = self._get_assistant_model(personality_dto["assistantModelId"])
 
     def _get_assistant_model(self, assistant_model_id: int) -> AssistantModel:
         successful, model = get_assistant_model(assistant_model_id)
