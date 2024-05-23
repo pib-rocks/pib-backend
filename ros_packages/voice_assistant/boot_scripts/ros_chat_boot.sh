a77f48a0586e16fc4abc38a4e6db8e122dd02134
Nino Meisinger <nino.meisinger@isento.de>
2024-05-23 13:09:55 +0200

    build: add ROS launch files

--- ros_packages/voice_assistant/boot_scripts/ros_chat_boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/pib/ros_working_dir/ros_config.sh
-source /opt/ros/humble/setup.bash
-source /home/pib/ros_working_dir/install/setup.bash
-ros2 run voice_assistant chat 2>&1 | tee -a ~/ros_working_dir/src/voice_assistant/chat.log
