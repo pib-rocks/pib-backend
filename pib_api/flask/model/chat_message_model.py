cdeada0a522f45915d735adbbabdb53ea70dfa8d
Nino Meisinger <nino.meisinger@isento.de>
2024-05-28 16:33:12 +0200

    fix: fix merge errors

--- pib_api/flask/model/chat_message_model.py
+++ pib_api/flask/model/chat_message_model.py
@@ -1,15 +1,16 @@
 from app.app import db
+from model.util import generate_uuid
 from sqlalchemy.sql import func
-import uuid
 
 
 class ChatMessage(db.Model):
-
     __tablename__ = "chatMessage"
 
     id = db.Column(db.Integer, primary_key=True)
-    messageId = db.Column(db.String(255), nullable=False, unique=True)
+    message_id = db.Column(
+        db.String(255), default=generate_uuid, nullable=False, unique=True
+    )
     timestamp = db.Column(db.DateTime, nullable=False, default=func.now())
-    isUser = db.Column(db.Boolean, nullable=False)
+    is_user = db.Column(db.Boolean, nullable=False)
     content = db.Column(db.String(100000), nullable=False)
-    chatId = db.Column(db.String(255), db.ForeignKey("chat.chatId"), nullable=False)
+    chat_id = db.Column(db.String(255), db.ForeignKey("chat.chat_id"), nullable=False)
