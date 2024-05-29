69d11115ff8ef2a52929bd55d902ff7d794e38c3
Nino Meisinger <nino.meisinger@isento.de>
2024-05-29 16:56:04 +0200

    fix: fix lint

--- pib_api/client/pib_api_client/voice_assistant_client.py
+++ pib_api/client/pib_api_client/voice_assistant_client.py
@@ -96,9 +96,12 @@ def create_chat_message(
 
 
 def get_all_chat_messages(chat_id: str) -> List[ChatMessage]:
-    request = Request(CHAT_MESSAGES_URL % chat_id, method='GET')
+    request = Request(CHAT_MESSAGES_URL % chat_id, method="GET")
     successful, chat_messages_dto = send_request(request)
-    if not successful: return successful, None
+    if not successful:
+        return successful, None
     chat_message_dtos = chat_messages_dto["messages"]
-    chat_messages = [ChatMessage(chat_message_dto) for chat_message_dto in chat_message_dtos]
+    chat_messages = [
+        ChatMessage(chat_message_dto) for chat_message_dto in chat_message_dtos
+    ]
     return successful, chat_messages
