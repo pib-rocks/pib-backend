2293e3133c5e0c3e177eb9795bd3085c33c88e06
nhe <nico.hertel@isento.de>
2024-06-24 13:39:25 +0200

    feat(PR-712): create display node

---
+++ ros_packages/display/launch/launch.py
@@ -0,0 +1,11 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(package="display", executable="display")
+        ]
+    )
