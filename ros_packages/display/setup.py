79b3122a7b238d6f5f9b93b269687fb0cdd88620
nhe <nico.hertel@isento.de>
2024-06-26 07:56:05 +0200

    fix(PR-712): setup.py of display-package finds launch file

--- ros_packages/display/setup.py
+++ ros_packages/display/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'display'
@@ -7,9 +10,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
