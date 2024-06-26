5886ff2318e5614214407d1943f58837ed3c696f
JaHe-isento <jan.hermann@isento.de>
2024-06-26 18:29:58 +0200

    refactor(update_webots_twin): move device initialization to function

--- ros_packages/pibsim_webots/pibsim_webots/pib_driver.py
+++ ros_packages/pibsim_webots/pibsim_webots/pib_driver.py
@@ -6,152 +6,83 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 class PibDriver:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
-
-        self.__devices = {}
-
-        self.__devices["turn_head_motor"] = [
-            self.__robot.getDevice("head_horizontal"),
-        ]
-        self.__devices["tilt_forward_motor"] = [
-            self.__robot.getDevice("head_vertical"),
-        ]
-        self.__devices["shoulder_horizontal_right"] = [
-            self.__robot.getDevice("shoulder_horizontal_right"),
-        ]
-        self.__devices["shoulder_horizontal_left"] = [
-            self.__robot.getDevice("shoulder_horizontal_left"),
-        ]
-        self.__devices["shoulder_vertical_right"] = [
-            self.__robot.getDevice("shoulder_vertical_right"),
-        ]
-        self.__devices["shoulder_vertical_left"] = [
-            self.__robot.getDevice("shoulder_vertical_left"),
-        ]
-        self.__devices["elbow_right"] = [
-            self.__robot.getDevice("elbow_right"),
-        ]
-        self.__devices["elbow_left"] = [
-            self.__robot.getDevice("elbow_left"),
-        ]
-
-        self.__devices["lower_arm_right_rotation"] = [
-            self.__robot.getDevice("forearm_right"),
-        ]
-        self.__devices["lower_arm_left_rotation"] = [
-            self.__robot.getDevice("forearm_left"),
-        ]
-        self.__devices["thumb_left_stretch"] = [
-            self.__robot.getDevice("thumb_left_distal"),
-            self.__robot.getDevice("thumb_left_proximal"),
-        ]
-        self.__devices["thumb_right_stretch"] = [
-            self.__robot.getDevice("thumb_right_distal"),
-            self.__robot.getDevice("thumb_right_proximal"),
-        ]
-        self.__devices["index_right_stretch"] = [
-            self.__robot.getDevice("index_right_distal"),
-            self.__robot.getDevice("index_right_proximal"),
-        ]
-        self.__devices["middle_right_stretch"] = [
-            self.__robot.getDevice("middle_right_distal"),
-            self.__robot.getDevice("middle_right_proximal"),
-        ]
-        self.__devices["ring_right_stretch"] = [
-            self.__robot.getDevice("ring_right_distal"),
-            self.__robot.getDevice("ring_right_proximal"),
-        ]
-        self.__devices["pinky_right_stretch"] = [
-            self.__robot.getDevice("pinky_right_distal"),
-            self.__robot.getDevice("pinky_right_proximal"),
-        ]
-        self.__devices["index_left_stretch"] = [
-            self.__robot.getDevice("index_left_distal"),
-            self.__robot.getDevice("index_left_proximal"),
-        ]
-        self.__devices["middle_left_stretch"] = [
-            self.__robot.getDevice("middle_left_distal"),
-            self.__robot.getDevice("middle_left_proximal"),
-        ]
-        self.__devices["ring_left_stretch"] = [
-            self.__robot.getDevice("ring_left_distal"),
-            self.__robot.getDevice("ring_left_proximal"),
-        ]
-        self.__devices["pinky_left_stretch"] = [
-            self.__robot.getDevice("pinky_left_distal"),
-            self.__robot.getDevice("pinky_left_proximal"),
-        ]
-
-        self.__devices["all_fingers_right"] = []
-        self.__devices["all_fingers_right"].extend(
-            self.__devices["thumb_right_stretch"]
-        )
-        self.__devices["all_fingers_right"].extend(
-            self.__devices["index_right_stretch"]
-        )
-        self.__devices["all_fingers_right"].extend(
-            self.__devices["middle_right_stretch"]
-        )
-        self.__devices["all_fingers_right"].extend(self.__devices["ring_right_stretch"])
-        self.__devices["all_fingers_right"].extend(
-            self.__devices["pinky_right_stretch"]
-        )
-
-        self.__devices["all_fingers_left"] = []
-        self.__devices["all_fingers_left"].extend(self.__devices["thumb_left_stretch"])
-        self.__devices["all_fingers_left"].extend(self.__devices["index_left_stretch"])
-        self.__devices["all_fingers_left"].extend(self.__devices["middle_left_stretch"])
-        self.__devices["all_fingers_left"].extend(self.__devices["ring_left_stretch"])
-        self.__devices["all_fingers_left"].extend(self.__devices["pinky_left_stretch"])
-
+        self.__devices = self.__initialize_devices()
         self.__target_trajectory = JointTrajectory()
 
         rclpy.init(args=None)
         self.__node = rclpy.create_node("pib_driver")
-        self.__node.get_logger().info("nach create node")
+        self.__node.get_logger().info("Node created")
         self.__node.create_subscription(
             JointTrajectory, "/joint_trajectory", self.__trajectory_callback, 1
         )
-        self.__node.get_logger().info("nach create subscribe")
+        self.__node.get_logger().info("Subscribed to /joint_trajectory")
+    
+    def __initialize_devices(self) -> dict:
+        device_groups = {
+            "turn_head_motor": ["head_horizontal"],
+            "tilt_forward_motor": ["head_vertical"],
+            "shoulder_horizontal_right": ["shoulder_horizontal_right"],
+            "shoulder_horizontal_left": ["shoulder_horizontal_left"],
+            "shoulder_vertical_right": ["shoulder_vertical_right"],
+            "shoulder_vertical_left": ["shoulder_vertical_left"],
+            "elbow_right": ["elbow_right"],
+            "elbow_left": ["elbow_left"],
+            "lower_arm_right_rotation": ["forearm_right"],
+            "lower_arm_left_rotation": ["forearm_left"],
+            "thumb_right_stretch": ["thumb_right_distal", "thumb_right_proximal"],
+            "thumb_left_stretch": ["thumb_left_distal", "thumb_left_proximal"],
+            "index_right_stretch": ["index_right_distal", "index_right_proximal"],
+            "middle_right_stretch": ["middle_right_distal", "middle_right_proximal"],
+            "ring_right_stretch": ["ring_right_distal", "ring_right_proximal"],
+            "pinky_right_stretch": ["pinky_right_distal", "pinky_right_proximal"],
+            "index_left_stretch": ["index_left_distal", "index_left_proximal"],
+            "middle_left_stretch": ["middle_left_distal", "middle_left_proximal"],
+            "ring_left_stretch": ["ring_left_distal", "ring_left_proximal"],
+            "pinky_left_stretch": ["pinky_left_distal", "pinky_left_proximal"],
+        }
 
-    def __trajectory_callback(self, trajectory):
+        devices = {}
+        for name, parts in device_groups.items():
+            devices[name] = [self.__robot.getDevice(part) for part in parts]
+
+        return devices
+    def __trajectory_callback(self, trajectory: JointTrajectory):
         self.__target_trajectory = trajectory
         self.__node.get_logger().info(
-            "trajectory callback "
-            + trajectory.joint_names[0]
-            + "  "
-            + str(trajectory.points[0].positions[0])
+            f"Trajectory received: moving {trajectory.joint_names[0]} to {trajectory.points[0].positions[0]}"
         )
 
-    def convert_cerebra(self, position):
+    def __convert_position(self, position: float) -> float:
         return math.radians(position / 100.0)
 
-    def create_device(self, name):
-        rotIndex = name.find("rota")
-        if rotIndex != -1:
-            device = name[: (rotIndex - 1)]
-        else:
-            device = name
-        self.__devices[name] = [
-            self.__robot.getDevice(device),
-        ]
-        self.__node.get_logger().info("created device: " + device + "   name: " + name)
+    def __create_device(self, name: str):
+        rot_index = name.find("rota")
+        device_name = name[:rot_index - 1] if rot_index != -1 else name
 
-    # position interface, only the first point in a trajectory is considered
-    def set_target_positions(self):
-        for index in range(len(self.__target_trajectory.joint_names)):
-            name = self.__target_trajectory.joint_names[index]
-            if not (name in self.__devices):
-                self.create_device(name)
+        try:
+            self.__devices[name] = [self.__robot.getDevice(device_name)]
+            self.__node.get_logger().info(f"Device created: {device_name} (name: {name})")
+        except Exception as e:
+            self.__node.get_logger().error(f"Failed to create device {name}: {str(e)}")
 
-            position = self.convert_cerebra(
-                self.__target_trajectory.points[0].positions[index]
-            )
+    def __set_target_positions(self):
+        if not self.__target_trajectory.joint_names:
+            return
 
-            if self.__devices[name] is not None:
-                for dev in self.__devices[name]:
-                    dev.setPosition(position)
+        for index, name in enumerate(self.__target_trajectory.joint_names):
+            if name not in self.__devices:
+                self.__create_device(name)
+
+            position = self.__convert_position(self.__target_trajectory.points[0].positions[index])
+
+            for device in self.__devices.get(name, []):
+                if device:
+                    try:
+                        device.setPosition(position)
+                    except Exception as e:
+                        self.__node.get_logger().error(f"Failed to set position for {name}: {str(e)}")
+
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
-
-        self.set_target_positions()
+        self.__set_target_positions()
