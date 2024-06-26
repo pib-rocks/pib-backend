ea7e0c86530b9d2f501786cc9b0e31a83e46d77a
Nino Meisinger <nino.meisinger@isento.de>
2024-06-26 13:18:48 +0200

    feat(PR-715): Unify setup script to support local/docker install

--- setup/installation_scripts/set_system_settings.sh
+++ setup/installation_scripts/set_system_settings.sh
@@ -3,9 +3,17 @@
 # This script adjust ubuntu system settings
 # To properly run this script relies on being sourced by the "setup-pib.sh"-script
 
-echo -e "$YELLOW_TEXT_COLOR""-- Adjusting system settings --""$RESET_TEXT_COLOR"	
+print INFO "Adjusting system settings"
 
-# This config file exists only on RaspberryPi, but not VMs 
+if ! [ "$DIST_VERSION" == "jammy" ]; then
+    print ERROR "Not using Ubuntu 22.04; skipping adjusting system settings"
+    return
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl enable ssh --now
+
+# This config file exists only on RaspberryPi, but not VMs
 CONFIG_FILE="/boot/firmware/config.txt"
 
 # Apply display and resolution settings if config file exists
@@ -30,10 +38,13 @@ if [ -e "$CONFIG_FILE" ]; then
             echo "${settingsMap[$setting]}" | sudo tee -a "$CONFIG_FILE" > /dev/null
         fi
     done
+else
+  print WARN "OS not running on Raspberry Pi; skipping adjusting display resolution"
 fi
 
 # Activate automatic login settings via regex
 sudo sed -i '/#  AutomaticLogin/{s/#//;s/user1/pib/}' /etc/gdm3/custom.conf
+print INFO "activated automatic login"
 
 # Disabling power saving settings
 gsettings set org.gnome.desktop.session idle-delay 0
@@ -43,7 +54,10 @@ gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
 
+print INFO "disabled power saving settings"
+
 # Add default ubuntu terminal to favorites
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'org.gnome.Terminal.desktop']"
+print INFO "added Terminal to favorites"
 
-echo -e "$NEW_LINE""$GREEN_TEXT_COLOR""-- System settings adjustment completed --""$RESET_TEXT_COLOR""$NEW_LINE"
+print SUCCESS "System settings adjusted"
