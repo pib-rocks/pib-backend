98e26d42776b3881e9bcd48ffddf1f97edc0c894
JaHe-isento <jan.hermann@isento.de>
2024-06-27 20:26:13 +0200

    refactor(update_webots_twin): completely rework the pib_driver
    removed unnecessary code, further modularized the script, updated motor name map, added documentation

--- ros_packages/pibsim_webots/pibsim_webots/pib_driver.py
+++ ros_packages/pibsim_webots/pibsim_webots/pib_driver.py
@@ -1,88 +1,99 @@
-import rclpy
 import math
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import rclpy
+from trajectory_msgs.msg import JointTrajectory
 
 
 class PibDriver:
+    """
+    This driver is the interface between pib's digital twin for Webots and the ROS jointTrajectory topic.
+    Every JointTrajectory message published to the topic gets converted into Webots movement instructions.
+    """
+
     def init(self, webots_node, properties):
+        """The init method is the Webot/ ROS node counterpart to the python __init__ constructor"""
         self.__robot = webots_node.robot
         self.__devices = self.__initialize_devices()
-        self.__target_trajectory = JointTrajectory()
 
         rclpy.init(args=None)
         self.__node = rclpy.create_node("pib_driver")
         self.__node.get_logger().info("Node created")
         self.__node.create_subscription(
-            JointTrajectory, "/joint_trajectory", self.__trajectory_callback, 1
+            JointTrajectory, "/joint_trajectory", self.__trajectory_callback, 10
         )
         self.__node.get_logger().info("Subscribed to /joint_trajectory")
-    
+
+    MOTOR_TO_DEVICES_MAP = {
+        "turn_head_motor": ["head_horizontal"],
+        "tilt_forward_motor": ["head_vertical"],
+        "upper_arm_left_rotation": ["upper_arm_left"],
+        "elbow_left": ["elbow_left"],
+        "lower_arm_left_rotation": ["forearm_left"],
+        "shoulder_vertical_left": ["shoulder_vertical_left"],
+        "shoulder_horizontal_left": ["shoulder_horizontal_left"],
+        "upper_arm_right_rotation": ["upper_arm_right"],
+        "elbow_right": ["elbow_right"],
+        "lower_arm_right_rotation": ["forearm_right"],
+        "shoulder_vertical_right": ["shoulder_vertical_right"],
+        "shoulder_horizontal_right": ["shoulder_horizontal_right"],
+        "thumb_right_opposition": ["thumb_right_opposition"],
+        "thumb_right_stretch": ["thumb_right_distal", "thumb_right_proximal"],
+        "index_right_stretch": ["index_right_distal", "index_right_proximal"],
+        "middle_right_stretch": ["middle_right_distal", "middle_right_proximal"],
+        "ring_right_stretch": ["ring_right_distal", "ring_right_proximal"],
+        "pinky_right_stretch": ["pinky_right_distal", "pinky_right_proximal"],
+        "thumb_left_opposition": ["thumb_left_opposition"],
+        "thumb_left_stretch": ["thumb_left_distal", "thumb_left_proximal"],
+        "index_left_stretch": ["index_left_distal", "index_left_proximal"],
+        "middle_left_stretch": ["middle_left_distal", "middle_left_proximal"],
+        "ring_left_stretch": ["ring_left_distal", "ring_left_proximal"],
+        "pinky_left_stretch": ["pinky_left_distal", "pinky_left_proximal"],
+        "wrist_left": ["wrist_left"],
+        "wrist_right": ["wrist_right"],
+    }
+
     def __initialize_devices(self) -> dict:
-        device_groups = {
-            "turn_head_motor": ["head_horizontal"],
-            "tilt_forward_motor": ["head_vertical"],
-            "shoulder_horizontal_right": ["shoulder_horizontal_right"],
-            "shoulder_horizontal_left": ["shoulder_horizontal_left"],
-            "shoulder_vertical_right": ["shoulder_vertical_right"],
-            "shoulder_vertical_left": ["shoulder_vertical_left"],
-            "elbow_right": ["elbow_right"],
-            "elbow_left": ["elbow_left"],
-            "lower_arm_right_rotation": ["forearm_right"],
-            "lower_arm_left_rotation": ["forearm_left"],
-            "thumb_right_stretch": ["thumb_right_distal", "thumb_right_proximal"],
-            "thumb_left_stretch": ["thumb_left_distal", "thumb_left_proximal"],
-            "index_right_stretch": ["index_right_distal", "index_right_proximal"],
-            "middle_right_stretch": ["middle_right_distal", "middle_right_proximal"],
-            "ring_right_stretch": ["ring_right_distal", "ring_right_proximal"],
-            "pinky_right_stretch": ["pinky_right_distal", "pinky_right_proximal"],
-            "index_left_stretch": ["index_left_distal", "index_left_proximal"],
-            "middle_left_stretch": ["middle_left_distal", "middle_left_proximal"],
-            "ring_left_stretch": ["ring_left_distal", "ring_left_proximal"],
-            "pinky_left_stretch": ["pinky_left_distal", "pinky_left_proximal"],
-        }
-
+        """Initialize all webot robot devices associated with the pib robot names"""
         devices = {}
-        for name, parts in device_groups.items():
-            devices[name] = [self.__robot.getDevice(part) for part in parts]
 
+        for pib_motor_name, proto_device_names in self.MOTOR_TO_DEVICES_MAP.items():
+            devices[pib_motor_name] = self.__find_devices_in_robot(proto_device_names)
         return devices
-    def __trajectory_callback(self, trajectory: JointTrajectory):
-        self.__target_trajectory = trajectory
-        self.__node.get_logger().info(
-            f"Trajectory received: moving {trajectory.joint_names[0]} to {trajectory.points[0].positions[0]}"
-        )
 
-    def __convert_position(self, position: float) -> float:
+    def __find_devices_in_robot(self, device_names: list) -> list:
+        """Retrieves devices from the robot based on a list of device names"""
+        device_list = []
+
+        for device_name in device_names:
+            device = self.__robot.getDevice(device_name)
+            if device is not None:
+                device_list.append(device)
+            else:
+                self.__node.get_logger().error(
+                    f"Failed to retrieve device: {device_name}"
+                )
+        return device_list
+
+    def convert_position(self, position: float) -> float:
+        """Converts degree to radian and adjust for cerebras offset (multiplying degree by 100)."""
         return math.radians(position / 100.0)
 
-    def __create_device(self, name: str):
-        rot_index = name.find("rota")
-        device_name = name[:rot_index - 1] if rot_index != -1 else name
+    def __trajectory_callback(self, joint_trajectory: JointTrajectory):
+        """Sets the positions of devices whenever a joint trajectory message is received."""
+        motor_name = joint_trajectory.joint_names[0]
+        device_target_position = self.convert_position(
+            joint_trajectory.points[0].positions[0]
+        )
 
-        try:
-            self.__devices[name] = [self.__robot.getDevice(device_name)]
-            self.__node.get_logger().info(f"Device created: {device_name} (name: {name})")
-        except Exception as e:
-            self.__node.get_logger().error(f"Failed to create device {name}: {str(e)}")
+        self.set_device_positions(motor_name, device_target_position)
+        self.__node.get_logger().info(
+            f"Device '{motor_name}' moved to position {device_target_position}"
+        )
 
-    def __set_target_positions(self):
-        if not self.__target_trajectory.joint_names:
-            return
-
-        for index, name in enumerate(self.__target_trajectory.joint_names):
-            if name not in self.__devices:
-                self.__create_device(name)
-
-            position = self.__convert_position(self.__target_trajectory.points[0].positions[index])
-
-            for device in self.__devices.get(name, []):
-                if device:
-                    try:
-                        device.setPosition(position)
-                    except Exception as e:
-                        self.__node.get_logger().error(f"Failed to set position for {name}: {str(e)}")
-
+    def set_device_positions(self, motor_name: str, device_target_position: float):
+        """Sets the positions of a devices."""
+        for device in self.__devices[motor_name]:
+            device.setPosition(device_target_position)
 
     def step(self):
+        """Spins the ROS node once to handle any pending messages or callbacks."""
         rclpy.spin_once(self.__node, timeout_sec=0)
-        self.__set_target_positions()
