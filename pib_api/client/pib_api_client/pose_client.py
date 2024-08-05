8a9444ae9ad221a40a3eb61a409741da6f789502
Nino Meisinger <nino.meisinger@isento.de>
2024-08-05 16:32:58 +0200

    chore(PR-716): Fix lint

--- pib_api/client/pib_api_client/pose_client.py
+++ pib_api/client/pib_api_client/pose_client.py
@@ -1,8 +1,10 @@
-from urllib.request import Request
-from pib_api_client import send_request, URL_PREFIX
 from typing import Any
+from urllib.request import Request
+
+from pib_api_client import send_request, URL_PREFIX
 
 MOTOR_POSITIONS_OF_POSE_URL = URL_PREFIX + "/pose/%s/motor-positions"
+
 
 def get_motor_positions_of_pose(pose_id) -> (bool, dict[str, Any]):
     request = Request(MOTOR_POSITIONS_OF_POSE_URL % pose_id, method="GET")
