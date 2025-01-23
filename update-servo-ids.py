7d6319d36d264e5f9f2669996e123b5930ee18f2
Elli <elisabeth.topilin@isento.de>
2025-01-23 13:57:54 +0100

    style(PR-804): format code and remove print

--- update-servo-ids.py
+++ update-servo-ids.py
@@ -3,6 +3,7 @@ import sqlite3
 database = "./pib_api/flask/pibdata.db"
 bricklet_numbers = [1, 2, 3]
 
+
 def ask_for_bricklet_uids() -> list[str]:
     new_uids = []
     for bricklet_number in bricklet_numbers:
@@ -12,29 +13,35 @@ def ask_for_bricklet_uids() -> list[str]:
         new_uids.append(user_uid)
     return new_uids
 
+
 def update_bricklet_uids() -> None:
     try:
         with sqlite3.connect(database) as conn:
             cur = conn.cursor()
-            
+
             new_uids = ask_for_bricklet_uids()
 
             for uid, bricklet_number in zip(new_uids, bricklet_numbers):
                 if not uid:
                     continue
-                cur.execute("UPDATE bricklet SET uid = ? WHERE (bricklet_number) = ?", (f"uid{bricklet_number}", bricklet_number),)
+                cur.execute(
+                    "UPDATE bricklet SET uid = ? WHERE (bricklet_number) = ?",
+                    (f"uid{bricklet_number}", bricklet_number),
+                )
 
             for uid, bricklet_number in zip(new_uids, bricklet_numbers):
                 if not uid:
                     continue
-                cur.execute("UPDATE bricklet SET uid = ? WHERE (bricklet_number) = ?", (uid, bricklet_number),)
+                cur.execute(
+                    "UPDATE bricklet SET uid = ? WHERE (bricklet_number) = ?",
+                    (uid, bricklet_number),
+                )
 
-            rows = cur.execute("SELECT uid, bricklet_number FROM bricklet").fetchall()
-            print(rows)
             print("New uids were successfully set")
     except Exception as e:
         print(e)
         print("New uids could not be set")
+
 
 if __name__ == "__main__":
     update_bricklet_uids()
