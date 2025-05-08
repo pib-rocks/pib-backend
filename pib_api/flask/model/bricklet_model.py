c97d22f525cf140a63eff6e213b2855796a4a198
Elli <elisabeth.topilin@isento.de>
2025-05-08 14:27:33 +0200

    feat(PR-871): add bricklet type to bricklet model

--- pib_api/flask/model/bricklet_model.py
+++ pib_api/flask/model/bricklet_model.py
@@ -7,8 +7,9 @@ class Bricklet(db.Model):
     __tablename__ = "bricklet"
 
     id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.String(30), nullable=True, default=generate_uuid, unique=True)
+    uid = db.Column(db.String(30), nullable=True, unique=True)
     bricklet_number = db.Column(db.Integer, nullable=False, unique=True)
+    type = db.Column(db.Enum("Solid State Relay Bricklet", "Servo Bricklet", name="bricklet_type"), nullable=False)
     bricklet_pins = db.relationship(
         BrickletPin, backref="bricklet", lazy=True, cascade="all,delete-orphan"
     )
