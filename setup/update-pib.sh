85183f7cf07b0587ca4405ab4a3e39367598fa96
Vlad Stavila <vladislav.stavila@isento.de>
2025-06-04 16:34:42 +0200

    refactor(PR-845): wrap the code in functions and make minor naming changes and lint fix

--- setup/update-pib.sh
+++ setup/update-pib.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
+#
+# This script assumes:
+#   - that setup-pib was already executed
+#   - the default-DEFAULT_USER "pib" is executing it
+
 set -e  # Stop on errors
 
 # Configuration
-USER="pib"
-LOG_FILE="/home/$USER/update.log"
+DEFAULT_USER="pib"
+LOG_FILE="$HOME/update-pib.log"
 
 # Check correct user
-if [ "$(whoami)" != "$USER" ]; then
-    echo "Run this as user: $USER"
+if [ "$(whoami)" != "$DEFAULT_USER" ]; then
+    echo "Run this as user: $DEFAULT_USER"
     exit 1
 fi
 
 # Setup sudo without password (temporary)
 echo "Setting up temporary sudo access..."
-sudo bash -c "echo '$USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$USER"
-sudo chmod 0440 "/etc/sudoers.d/$USER"
+sudo bash -c "echo '$DEFAULT_USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$DEFAULT_USER"
+sudo chmod 0440 "/etc/sudoers.d/$DEFAULT_USER"
 
 # Start logging
 echo "Logging to $LOG_FILE"
 exec > >(tee -a "$LOG_FILE") 2>&1
 echo -e "\nUpdate started: $(date)"
 
+
+function update_backend() {
+    echo -e "\nUpdating backend:"
+    cd "$BACKEND_DIR"
+    git pull
+    sudo docker compose --profile all up --force-recreate --build -d
+}
+
+function update_frontend() {
+    echo -e "\nUpdating frontend:"
+    cd "$FRONTEND_DIR"
+    git pull
+    sudo docker compose up --force-recreate --build -d
+}
+
 # Update backend
-echo -e "\nUpdating backend:"
-cd "$BACKEND_DIR"
-git pull
-sudo docker compose --profile all up --force-recreate --build -d
+update_backend
 
 # Update frontend
-echo -e "\nUpdating frontend:"
-cd "$FRONTEND_DIR"
-git pull
-sudo docker compose up --force-recreate --build -d
+update_frontend
+
 
 # Cleanup
 echo -e "\nCleaning up:"
-sudo rm -v "/etc/sudoers.d/$USER"
+sudo rm -v "/etc/sudoers.d/$DEFAULT_USER"
 
 echo -e "\nUpdate successful."
