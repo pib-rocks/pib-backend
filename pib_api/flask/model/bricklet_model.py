c4077dc158d0c2c474a6755d7cf5ad55981578f6
Elli <elisabeth.topilin@isento.de>
2025-06-05 08:42:15 +0200

    chore(PR-871): fix linting

--- pib_api/flask/model/bricklet_model.py
+++ pib_api/flask/model/bricklet_model.py
@@ -9,7 +9,10 @@ class Bricklet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.String(30), nullable=True, unique=True)
     bricklet_number = db.Column(db.Integer, nullable=False, unique=True)
-    type = db.Column(db.Enum("Solid State Relay Bricklet", "Servo Bricklet", name="bricklet_type"), nullable=False)
+    type = db.Column(
+        db.Enum("Solid State Relay Bricklet", "Servo Bricklet", name="bricklet_type"),
+        nullable=False,
+    )
     bricklet_pins = db.relationship(
         BrickletPin, backref="bricklet", lazy=True, cascade="all,delete-orphan"
     )
