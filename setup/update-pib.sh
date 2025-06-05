2c6280c7d3cd57abfcd31fd03ec9de66377f7b84
Vlad Stavila <vladislav.stavila@isento.de>
2025-06-05 12:35:37 +0200

    refactor(PR-845): change color of prints and fix comments

--- setup/update-pib.sh
+++ setup/update-pib.sh
@@ -2,7 +2,7 @@
 #
 # This script assumes:
 #   - that setup-pib was already executed
-#   - the default-DEFAULT_USER "pib" is executing it
+#   - the user "pib" is executing it
 
 # Color definitions for logging
 export ERROR="\e[31m"
@@ -38,56 +38,53 @@ function print() {
     echo -e "${!color}[$(date -u)][[ ${text} ]]${RESET_TEXT_COLOR}"
 }
 
-# Check correct user
-if [ "$(whoami)" != "$DEFAULT_USER" ]; then
-    print "Run this as user: $DEFAULT_USER"
-    exit 1
-fi
-
-# Setup sudo without password (temporary)
-print "Setting up temporary sudo access..."
-sudo bash -c "echo '$DEFAULT_USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$DEFAULT_USER"
-sudo chmod 0440 "/etc/sudoers.d/$DEFAULT_USER"
-
-# Start logging
-print "Logging to $LOG_FILE"
-exec > >(tee -a "$LOG_FILE") 2>&1
-print "Update started: "
-
-
 function update_backend() {
     if [ -d "$BACKEND_DIR" ]; then
-        print "\nUpdating backend:"
-        cd "$BACKEND_DIR" || { echo -e "Cannot get to $BACKEND_DIR"; exit 1; }
-        git pull || { echo -e "backend git pull error"; exit 1; }
-        sudo docker compose --profile all up --force-recreate --build -d || { echo -e "docker compose backend build error"; exit 1; }
+        print INFO "Updating backend:"
+        cd "$BACKEND_DIR" || { print ERROR "Cannot get to $BACKEND_DIR"; exit 1; }
+        git pull || { print ERROR "backend git pull error"; exit 1; }
+        sudo docker compose --profile all up --force-recreate --build -d || { print ERROR "docker compose backend build error"; exit 1; }
     else
-        print "Directory $BACKEND_DIR does not exist"
+        print ERROR "Directory $BACKEND_DIR does not exist"
         exit 1 
     fi
 }
 
 function update_frontend() {
     if [ -d "$FRONTEND_DIR" ]; then
-        print "\nUpdating frontend:"
-        cd "$FRONTEND_DIR" || { echo -e "Cannot get to $FRONTEND_DIR"; exit 1; }
-        git pull || { echo -e "frontend git pull error"; exit 1; }
-        sudo docker compose up --force-recreate --build -d || { echo -e "docker compose frontend build error"; exit 1; }
+        print INFO "Updating frontend:"
+        cd "$FRONTEND_DIR" || { print ERROR "Cannot get to $FRONTEND_DIR"; exit 1; }
+        git pull || { print ERROR "frontend git pull error"; exit 1; }
+        sudo docker compose up --force-recreate --build -d || { print ERROR "docker compose frontend build error"; exit 1; }
     else
-        print "Directory $FRONTEND_DIR does not exist"
+        print ERROR "Directory $FRONTEND_DIR does not exist"
         exit 1 
     fi
 }
 
-# Update backend
+# Check correct user
+if [ "$(whoami)" != "$DEFAULT_USER" ]; then
+    print INFO "Run this as user: $DEFAULT_USER"
+    exit 1
+fi
+
+# Setup sudo without password (temporary)
+print INFO "Setting up temporary sudo access..."
+sudo bash -c "echo '$DEFAULT_USER ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$DEFAULT_USER"
+sudo chmod 0440 "/etc/sudoers.d/$DEFAULT_USER"
+
+# Start logging
+print INFO "Logging to $LOG_FILE"
+exec > >(tee -a "$LOG_FILE") 2>&1
+print INFO "Update started: "
+
 update_backend
 
-# Update frontend
 update_frontend
 
 
 # Cleanup
-print "Cleaning up:"
+print INFO "Cleaning up:"
 sudo rm -v "/etc/sudoers.d/$DEFAULT_USER"
 
-print "Update successful."
+print SUCCESS "Update successful."
