f2dfdcb98094a315a703b2929ce663837cfafc68
Elli <elisabeth.topilin@isento.de>
2025-06-25 15:49:32 +0200

    feat(PR-872): add set_ssr_state function

--- ros_packages/motors/pib_motors/pib_motors/bricklet.py
+++ ros_packages/motors/pib_motors/pib_motors/bricklet.py
@@ -1,10 +1,12 @@
 import os
+import logging
 
 from pib_api_client import bricklet_client
 from pib_motors.config import cfg
 from tinkerforge.brick_hat import BrickHAT
 from tinkerforge.bricklet_servo_v2 import BrickletServoV2
-from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_solid_state_relay_v2 import BrickletSolidStateRelayV2
+from tinkerforge.ip_connection import IPConnection, Error
 
 TINKERFORGE_HOST = os.getenv("TINKERFORGE_HOST", "localhost")
 TINKERFORGE_PORT = int(os.getenv("TINKERFORGE_PORT", 4223))
@@ -18,9 +20,39 @@ ipcon.connect(cfg.TINKERFORGE_HOST, cfg.TINKERFORGE_PORT)
 successful, bricklet_dtos = bricklet_client.get_all_bricklets()
 if not successful:
     raise RuntimeError("failed to load bricklets from pib-api...")
-bricklet_uids = [dto["uid"] for dto in bricklet_dtos["bricklets"]]
 
-# maps the uid (e.g. 'XYZ') to the associated bricklet object
-uid_to_bricklet: dict[str, BrickletServoV2] = {
-    uid: BrickletServoV2(uid, ipcon) for uid in bricklet_uids
+servo_bricklet_uids = [dto["uid"] for dto in bricklet_dtos["bricklets"] if dto["type"] == "Servo Bricklet"]
+solid_state_relay_bricklet_uid = next(
+    (dto["uid"] for dto in bricklet_dtos["bricklets"] if dto["type"] == "Solid State Relay Bricklet" and dto["uid"]),
+    None
+)
+
+# maps the uid (e.g. 'XYZ') to the associated servo bricklet object
+uid_to_servo_bricklet: dict[str, BrickletServoV2] = {
+    uid: BrickletServoV2(uid, ipcon) for uid in servo_bricklet_uids if uid
 }
+
+# maps the uid (e.g. 'XYZ') to the associated solid state relay bricklet object
+if solid_state_relay_bricklet_uid:
+    solid_state_relay_bricklet: BrickletSolidStateRelayV2 = BrickletSolidStateRelayV2(solid_state_relay_bricklet_uid[0], ipcon)
+
+def set_ssr_state(state: bool) -> None:
+    """set the status of the solid state relay to on (true) or off (false)"""
+    if solid_state_relay_bricklet_uid is None:
+        logging.error("SSR uid is not set")
+        return
+    else:
+        try:
+            solid_state_relay_bricklet.set_state(state)
+            logging.info(f"Solid-State Relay '{status}'")
+        except Error as e:
+            if e.value == Error.TIMEOUT:
+                logging.error("Solid-State Relay is not connected or unresponsive (Timeout).")
+            elif e.value == Error.INVALID_UID:
+                logging.error("Invalid UID for Solid-State Relay.")
+            elif e.value == Error.NOT_CONNECTED:
+                logging.error("No connection to Solid-State Relay.")
+            else:
+                logging.error(f"Unknown Tinkerforge error: {e}")
+        except Exception as e:
+            logging.error(f"Unexpected error while setting SSR state: {e}")
