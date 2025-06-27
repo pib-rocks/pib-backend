e7536af91b89b36d22623cffb1649576981cb5f5
Elli <elisabeth.topilin@isento.de>
2025-06-27 10:19:59 +0200

    style(PR-872): formatting

--- ros_packages/motors/pib_motors/pib_motors/bricklet.py
+++ ros_packages/motors/pib_motors/pib_motors/bricklet.py
@@ -21,10 +21,16 @@ successful, bricklet_dtos = bricklet_client.get_all_bricklets()
 if not successful:
     raise RuntimeError("failed to load bricklets from pib-api...")
 
-servo_bricklet_uids = [dto["uid"] for dto in bricklet_dtos["bricklets"] if dto["type"] == "Servo Bricklet"]
+servo_bricklet_uids = [
+    dto["uid"] for dto in bricklet_dtos["bricklets"] if dto["type"] == "Servo Bricklet"
+]
 solid_state_relay_bricklet_uid = next(
-    (dto["uid"] for dto in bricklet_dtos["bricklets"] if dto["type"] == "Solid State Relay Bricklet" and dto["uid"]),
-    None
+    (
+        dto["uid"]
+        for dto in bricklet_dtos["bricklets"]
+        if dto["type"] == "Solid State Relay Bricklet" and dto["uid"]
+    ),
+    None,
 )
 
 # maps the uid (e.g. 'XYZ') to the associated servo bricklet object
@@ -34,7 +40,10 @@ uid_to_servo_bricklet: dict[str, BrickletServoV2] = {
 
 # maps the uid (e.g. 'XYZ') to the associated solid state relay bricklet object
 if solid_state_relay_bricklet_uid:
-    solid_state_relay_bricklet: BrickletSolidStateRelayV2 = BrickletSolidStateRelayV2(solid_state_relay_bricklet_uid, ipcon)
+    solid_state_relay_bricklet: BrickletSolidStateRelayV2 = BrickletSolidStateRelayV2(
+        solid_state_relay_bricklet_uid, ipcon
+    )
+
 
 def set_ssr_state(state: bool) -> None:
     """set the status of the solid state relay to on (true) or off (false)"""
@@ -47,7 +56,9 @@ def set_ssr_state(state: bool) -> None:
             logging.info(f"Solid-State Relay {'ON' if state else 'OFF'}")
         except Error as e:
             if e.value == Error.TIMEOUT or e.value == Error.WRONG_RESPONSE_LENGTH:
-                logging.error("Solid-State Relay is not connected or unresponsive (Timeout). Please check the UID and ensure the device is plugged in.")
+                logging.error(
+                    "Solid-State Relay is not connected or unresponsive (Timeout). Please check the UID and ensure the device is plugged in."
+                )
             elif e.value == Error.INVALID_UID:
                 logging.error("Invalid UID for Solid-State Relay.")
             elif e.value == Error.NOT_CONNECTED:
