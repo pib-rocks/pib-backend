c4e9253d0297b0686ad7487257d51835e46ffa06
Elli <elisabeth.topilin@isento.de>
2025-07-22 11:50:48 +0200

    feat(PR-568): add creation of ip_dispatcher-script during setup and add endpoint for getting the host-ip

---
+++ pib_api/flask/controller/ip_controller.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+import os
+
+bp = Blueprint("ip_controller", __name__)
+
+@bp.route("", methods=["GET"])
+def get_host_ip():
+    ip_file = "/app/host_ip.txt"
+    if os.path.exists(ip_file):
+        with open(ip_file, "r") as f:
+            ip = f.read().strip()
+            return jsonify({"host_ip": ip}), 200
+    else:
+        return jsonify({"host_ip": ""}), 200
