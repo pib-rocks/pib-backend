c2a878146d50ad1578226111cbba1d677a549333
Elli <elisabeth.topilin@isento.de>
2025-07-30 16:51:35 +0200

    fix(PR-568): fix location of host-ip.txt

--- pib_api/flask/controller/ip_controller.py
+++ pib_api/flask/controller/ip_controller.py
@@ -7,7 +7,7 @@ bp = Blueprint("ip_controller", __name__)
 
 @bp.route("", methods=["GET"])
 def get_host_ip():
-    ip_file = app.config.get("HOST_IP_FILE", "/app/host_ip.txt")
+    ip_file = app.config.get("HOST_IP_FILE", "host_ip.txt")
     if os.path.exists(ip_file):
         with open(ip_file, "r") as f:
             ip = f.read().strip()
