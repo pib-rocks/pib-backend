2734ceb49265ab5243601069b2c0b21164692277
Elli <elisabeth.topilin@isento.de>
2025-12-01 11:28:13 +0100

    refactor(PR-961): remove unused motor constants and import

--- pib_api/flask/migrations/versions/e125aa01171c_add_deletable_to_pose.py
+++ pib_api/flask/migrations/versions/e125aa01171c_add_deletable_to_pose.py
@@ -6,7 +6,6 @@ Create Date: 2025-09-11 13:54:41.677586
 
 """
 
-from model.util import generate_uuid
 from alembic import op
 import sqlalchemy as sa
 
@@ -16,70 +15,6 @@ revision = "e125aa01171c"
 down_revision = "ddd0daa8e6f5"
 branch_labels = None
 depends_on = None
-
-all_motors = [
-    "turn_head_motor",
-    "tilt_forward_motor",
-    "upper_arm_left_rotation",
-    "elbow_left",
-    "lower_arm_left_rotation",
-    "shoulder_vertical_left",
-    "shoulder_horizontal_left",
-    "upper_arm_right_rotation",
-    "elbow_right",
-    "lower_arm_right_rotation",
-    "shoulder_vertical_right",
-    "shoulder_horizontal_right",
-    "thumb_right_opposition",
-    "thumb_right_stretch",
-    "index_right_stretch",
-    "middle_right_stretch",
-    "ring_right_stretch",
-    "pinky_right_stretch",
-    "thumb_left_opposition",
-    "thumb_left_stretch",
-    "index_left_stretch",
-    "middle_left_stretch",
-    "ring_left_stretch",
-    "pinky_left_stretch",
-    "wrist_left",
-    "wrist_right",
-]
-
-overrides_startup = {
-    "elbow_left": 5500,
-    "elbow_right": 5500,
-    "thumb_right_opposition": -9000,
-    "thumb_right_stretch": -9000,
-    "index_right_stretch": -9000,
-    "middle_right_stretch": -9000,
-    "ring_right_stretch": -9000,
-    "pinky_right_stretch": -9000,
-    "thumb_left_opposition": -9000,
-    "thumb_left_stretch": -9000,
-    "index_left_stretch": -9000,
-    "middle_left_stretch": -9000,
-    "ring_left_stretch": -9000,
-    "pinky_left_stretch": -9000,
-    "shoulder_vertical_left": -9000,
-    "shoulder_horizontal_left": -9000,
-    "shoulder_vertical_right": -9000,
-    "shoulder_horizontal_right": -9000,
-}
-overrides_calibration = {
-    "thumb_right_opposition": -9000,
-    "thumb_right_stretch": -9000,
-    "index_right_stretch": -9000,
-    "middle_right_stretch": -9000,
-    "ring_right_stretch": -9000,
-    "pinky_right_stretch": -9000,
-    "thumb_left_opposition": -9000,
-    "thumb_left_stretch": -9000,
-    "index_left_stretch": -9000,
-    "middle_left_stretch": -9000,
-    "ring_left_stretch": -9000,
-    "pinky_left_stretch": -9000,
-}
 
 
 def upgrade():
