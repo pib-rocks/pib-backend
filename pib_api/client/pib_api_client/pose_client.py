b3cfab38deda49ccb33c5a38d4b6caf3699481e2
Elli <elisabeth.topilin@isento.de>
2026-02-10 16:32:25 +0100

    fix(PR-962): update return type hint in pose_client

--- pib_api/client/pib_api_client/pose_client.py
+++ pib_api/client/pib_api_client/pose_client.py
@@ -8,12 +8,12 @@ MOTOR_POSITIONS_OF_POSE_URL = URL_PREFIX + "/pose/%s/motor-positions"
 POSE_BY_NAME_URL = URL_PREFIX + "/pose/by-name/%s"
 
 
-def get_motor_positions_of_pose(pose_id) -> (bool, dict[str, Any]):
+def get_motor_positions_of_pose(pose_id) -> tuple[bool, dict[str, Any] | None]:
     request = Request(MOTOR_POSITIONS_OF_POSE_URL % pose_id, method="GET")
     return send_request(request)
 
 
-def get_pose_by_name(pose_name: str) -> (bool, dict[str, Any]):
+def get_pose_by_name(pose_name: str) -> tuple[bool, dict[str, Any] | None]:
     encoded_name = quote(pose_name, safe="")
     request = Request(POSE_BY_NAME_URL % encoded_name, method="GET")
     return send_request(request)
